353b51327b0d3b2907b57bab7d84bc438363a792
Logan Henson <logan@loganhenson.com>
2017-04-12 16:09:58 -0500

    remove leading slashes from routes

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,27 @@
 
 use App\JATO\Make;
 use App\JATO\Version;
+use App\SavedVehicle;
 use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('', function () {
     return view('step-0')
         ->with('makes', Make::select('id', 'name')->with(['models' => function ($query) {
             $query->select('id', 'name', 'make_id');
         }])->get());
 });
 
-Route::post('/step-0', function () {
+Route::post('step-0', function () {
     $versions = Version::where('model_id', request()->input('model_id'))->get();
 
     return view('step-1-versions')->with('versions', $versions);
 });
 
-Route::post('/step-1', function () {
+Route::post('step-1', function () {
     $options = \App\JATO\VersionOption::where('version_id', request()->input('version_id'))->get();
 
     return view('step-2-options')
@@ -29,7 +30,7 @@ Route::post('/step-1', function () {
         ->with('versionId', request()->input('version_id'));
 });
 
-Route::post('/step-2', function () {
+Route::post('step-2', function () {
     $options = \App\JATO\VersionOption::where('version_id', request()->input('version_id'))->get();
     $version = Version::findOrFail(request()->input('version_id'));
 
@@ -39,7 +40,7 @@ Route::post('/step-2', function () {
         ->with('version', $version);
 });
 
-Route::post('/save', function () {
+Route::post('save', function () {
     /** @var \App\User $user */
     $user = \App\User::firstOrCreate([
         'email' => request()->input('email'),
@@ -49,8 +50,8 @@ Route::post('/save', function () {
         'password' => Hash::make(str_random(8))
     ]);
 
-    /** @var \App\SavedVehicle $savedVehicle */
-    $savedVehicle = \App\SavedVehicle::create([
+    /** @var SavedVehicle $savedVehicle */
+    $savedVehicle = SavedVehicle::create([
         'user_id' => $user->id,
         'version_id' => request()->input('version_id')
     ]);
