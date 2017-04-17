f19e1ab817aff8bd3891c0899da7f17db189372a
Logan Henson <logan@loganhenson.com>
2017-04-17 13:17:29 -0500

    step-3 style review

--- resources/views/step-3-buy-or-save.blade.php
+++ resources/views/step-3-buy-or-save.blade.php
@@ -17,39 +17,15 @@
         <form method="post" action="/buy">
             {{ csrf_field() }}
 
-            @foreach ($options as $option)
-                <div class="step-3__option {{ in_array($option->id, $selectedOptionIds) ? '' : 'hide' }}">
-                    <label>
-                        <input disabled {{ in_array($option->id, $selectedOptionIds) ? 'checked' : '' }} type="checkbox" name="option_ids[]" value="{{ $option->id }}">
-                        <strong>{{ $option->name }}</strong>
-                    </label>
-                </div>
-            @endforeach
-
-            @foreach ($options as $option)
-                @if (in_array($option->id, $selectedOptionIds))
-                    <input type="hidden" name="option_ids[]" value="{{ $option->id }}">
-                @endif
-            @endforeach
-
-            <input type="hidden" name="version_id" value="{{ $version->id }}">
-
-            <label>
-                <button class="btn btn-primary" type="submit">Make this my ride</button>
-            </label>
-        </form>
-
-        <br>
-        <br>
-
-        <form method="post" action="{{ route('savedVehicle.store') }}">
-            {{ csrf_field() }}
-
-            <div class="form-group">
-                <label>Share your car with yourself, or come back later to view it
-                    <br>
-                    <input class="form-control" type="email" name="email" required>
-                </label>
+            <div class="options">
+                @foreach ($options as $option)
+                    <div class="options__option {{ in_array($option->id, $selectedOptionIds) ? '' : 'hide' }}">
+                        <label>
+                            <input disabled {{ in_array($option->id, $selectedOptionIds) ? 'checked' : '' }} type="checkbox" name="option_ids[]" value="{{ $option->id }}">
+                            <strong>{{ $option->name }}</strong>
+                        </label>
+                    </div>
+                @endforeach
             </div>
 
             @foreach ($options as $option)
@@ -60,7 +36,40 @@
 
             <input type="hidden" name="version_id" value="{{ $version->id }}">
 
-            <button class="btn btn-primary" type="submit">Save</button>
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <button class="btn btn-primary" type="submit">Make this my ride</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+
+        <hr>
+
+        <form method="post" action="{{ route('savedVehicle.store') }}">
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <label for="email">Share your car with yourself, or come back later to view it</label>
+                        <input class="form-control" type="email" name="email" required>
+
+                        <br>
+
+                        <button class="btn btn-primary" type="submit">Save to garage</button>
+                    </div>
+                </div>
+            </div>
+
+            @foreach ($options as $option)
+                @if (in_array($option->id, $selectedOptionIds))
+                    <input type="hidden" name="option_ids[]" value="{{ $option->id }}">
+                @endif
+            @endforeach
+
+            <input type="hidden" name="version_id" value="{{ $version->id }}">
         </form>
     </div>
 @endsection
