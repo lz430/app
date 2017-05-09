9d998199177154b987fa23aa9551836ae34350c4
Jose Soto <josecanhelp@gmail.com>
2017-05-09 11:19:39 -0700

    Add API Endpoint for Makes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::get('makes', 'API\V1\MakesController@index');
+});
