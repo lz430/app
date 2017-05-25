298e46eb051af56c5c20d7892bebd38671478320
Logan Henson <logan@loganhenson.com>
2017-05-25 13:37:20 -0500

    add application form

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ Route::group(['middleware' => 'auth'], function () {
     /** Purchase Flow */
     Route::get('apply-or-purchase', 'ApplyOrPurchaseController@applyOrPurchase')->name('applyOrPurchase');
     Route::post('purchase', 'ApplyOrPurchaseController@purchase')->name('purchase');
+    Route::get('apply', 'ApplyOrPurchaseController@viewApply')->name('viewApply');
+    Route::post('apply', 'ApplyOrPurchaseController@apply')->name('apply');
 });
 
 Route::get('buy-request/thanks', function () {
