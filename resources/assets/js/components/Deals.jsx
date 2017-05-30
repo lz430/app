0c014f66396f341401f5b5a946a67a0f1c1dabbd
Jose Soto <josecanhelp@gmail.com>
2017-05-30 16:09:47 -0700

    Convert Deals to stateless component

--- resources/assets/js/components/Deals.jsx
+++ resources/assets/js/components/Deals.jsx
@@ -2,21 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import Deal from './Deal'
 
-class Deals extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const deals = this.props.deals;
-
-        return <div className="deals">
-            {deals.data.map((deal, index) => {
-                return <Deal deal={deal} key={index}></Deal>
-            })}
-        </div>
-    }
-}
+const Deals = ({deals}) => {
+    return <div className="deals">
+        {deals.data.map((deal, index) => {
+            return <Deal deal={deal} key={index}></Deal>
+        })}
+    </div>
+};
 
 Deals.propTypes = {
     deals: PropTypes.shape({
