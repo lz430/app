e33f3656266afda3fedbd72579f4a8d141d2a659
Logan Henson <logan@loganhenson.com>
2017-05-31 08:39:24 -0500

    add link on filter page -> apply-or-purchase for deal

--- resources/assets/js/components/Deal.jsx
+++ resources/assets/js/components/Deal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import R from 'ramda';
-import util from '../src/util'
+import util from '../src/util';
 
 class Deal extends React.Component {
     constructor(props) {
@@ -12,14 +12,19 @@ class Deal extends React.Component {
         return (
             <div className="deal">
                 <div className="deal__basic-info">
-                    <p>{`${deal.year} ${deal.make} ${deal.model}`}
+                    <p>
+                        <a href={`apply-or-purchase?deal_id=${deal.id}`}>{`${deal.year} ${deal.make} ${deal.model}`}</a>
                         <br />
                         <strong>{util.moneyFormat(deal.msrp)} MSRP</strong>
                     </p>
                 </div>
                 <img
                     className="deal__image"
-                    src={R.propOr('/images/dmr-logo.svg', 'url', deal.photos.data[0])}
+                    src={R.propOr(
+                        '/images/dmr-logo.svg',
+                        'url',
+                        deal.photos.data[0]
+                    )}
                 />
                 <div className="deal__buttons">
                     <button>Details</button>
