90845d1ecb6a6c8f9bc83d3c92c9c8aa125fc0a4
Jose Soto <josecanhelp@gmail.com>
2017-06-20 09:23:39 -0700

    Refactor to CompareTitleBar

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,6 +5,7 @@ import Configurator from 'containers/Configurator';
 import FilterPage from 'containers/FilterPage';
 import { Provider } from 'react-redux';
 import store from 'configureStore';
+import CompareTitleBar from 'components/CompareTitleBar';
 
 const filterStore = store();
 
@@ -25,4 +26,11 @@ Array.from(document.getElementsByTagName('FilterPage')).map(element => {
         </Provider>,
         element
     );
+});
+
+/**
+ * CompareTitleBar
+ */
+Array.from(document.getElementsByTagName('CompareTitleBar')).map(element => {
+    ReactDOM.render(<CompareTitleBar />, element);
 });
