9bb167e8e2221ee04af152292e480fb65718e460
Jose Soto <josecanhelp@gmail.com>
2017-07-18 13:33:59 -0700

    WIP

--- app/Http/Controllers/API/DealsController.php
+++ app/Http/Controllers/API/DealsController.php
@@ -106,7 +106,7 @@ class DealsController extends BaseAPIController
             });
         })->whereNotNull('price')->whereNotNull('msrp')->with(['photos' => function ($query) {
             $query->orderBy('id');
-        }]);
+        },]);
     }
 
     private function filterQueryByTransmissionType(Builder $query, Request $request) : Builder
