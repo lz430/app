478fbdd6e077ec4b212e1e2a9c9e5e492c438b23
Jose Soto <josecanhelp@gmail.com>
2017-08-07 09:55:53 -0700

    Correct formatting and syntax

--- tests/Unit/DealsFilterTest.php
+++ tests/Unit/DealsFilterTest.php
@@ -15,12 +15,12 @@ class DealsFilterTest extends TestCase
         $deal = factory(Deal::class)->create();
         
         factory(Purchase::class)->create([
-        'type' => 'cash',
-        'deal_id' => $deal->id,
-        'dmr_price' => 30000,
-        'msrp' => 28000,
-        'completed_at' => Carbon::now(),
-    ]);
+            'type' => 'cash',
+            'deal_id' => $deal->id,
+            'dmr_price' => 30000,
+            'msrp' => 28000,
+            'completed_at' => Carbon::now(),
+        ]);
         
         $this->assertEquals(0, Deal::forSale()->count());
     
