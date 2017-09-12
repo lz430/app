d1c4d0939fcaecba400623c1c9619cdc6f0b41f7
Logan Henson <logan@loganhenson.com>
2017-09-12 14:25:49 -0500

    setup redis + cache for rebates endpoint, remove  method (unused)

--- app/Http/Controllers/API/RebatesController.php
+++ app/Http/Controllers/API/RebatesController.php
@@ -6,6 +6,8 @@ use App\Deal;
 use App\Http\Controllers\Controller;
 use DeliverMyRide\JATO\Client;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class RebatesController extends Controller
 {
@@ -19,86 +21,53 @@ class RebatesController extends Controller
             'selected_rebate_ids' => 'array:int',
         ]);
 
-        $vin = request('vin');
-        $zipcode = request('zipcode');
+        $cacheKey = __FUNCTION__
+            . ':' . request('zipcode')
+            . ':' . request('vin')
+            . ':' . implode(request('selected_rebate_ids', []));
 
-        $version = Deal::where('vin', $vin)->with('versions')->firstOrFail()->versions->first();
-        $vehicleId = $version->jato_vehicle_id;
+        return Cache::remember($cacheKey, 1440, function () use ($Client) {
+            $vin = request('vin');
+            $zipcode = request('zipcode');
 
-        $incentives = request()->has('selected_rebate_ids')
-            ? collect($Client->incentivesByVehicleIdAndZipcodeWithSelected(
-                $vehicleId,
-                $zipcode,
-                request('selected_rebate_ids')
-            ))
-            : collect($Client->incentivesByVehicleIdAndZipcode($vehicleId, $zipcode));
+            $version = Deal::where('vin', $vin)->with('versions')->firstOrFail()->versions->first();
+            $vehicleId = $version->jato_vehicle_id;
 
-        $availableRebates = $incentives
-            ->filter(function ($incentive) {
-                return in_array($incentive['categoryName'], [
-                    "Retail Cash Programs",
-                ]);
-            })->map(function ($incentive) {
-                return [
-                    'id' => $incentive['subProgramId'],
-                    'rebate' => $incentive['restrictions'],
-                    'value' => $incentive['cash'],
-                    'statusName' => $incentive['statusName'],
-                    'types' => [
-                        'Auto Show Cash' => ['cash', 'finance'],
-                        'Bonus Cash' => ['cash', 'finance'],
-                        'Cash Back' => ['cash', 'finance'],
-                        'Credit Card Rebate' => ['cash', 'finance'],
-                        'Cash on MSRP ' => ['cash', 'finance'],
-                        'Cash on Term APR' => ['finance'],
-                    ][$incentive['typeName']] ?? [],
-                ];
-            })->filter(function ($incentive) {
-                return ! empty($incentive['types']);
-            })->values();
+            $incentives = request()->has('selected_rebate_ids')
+                ? collect($Client->incentivesByVehicleIdAndZipcodeWithSelected(
+                    $vehicleId,
+                    $zipcode,
+                    request('selected_rebate_ids')
+                ))
+                : collect($Client->incentivesByVehicleIdAndZipcode($vehicleId, $zipcode));
 
-        return response()->json([
-            'rebates' => $availableRebates,
-        ]);
-    }
+            $availableRebates = $incentives
+                ->filter(function ($incentive) {
+                    return in_array($incentive['categoryName'], [
+                        "Retail Cash Programs",
+                    ]);
+                })->map(function ($incentive) {
+                    return [
+                        'id' => $incentive['subProgramId'],
+                        'rebate' => $incentive['restrictions'],
+                        'value' => $incentive['cash'],
+                        'statusName' => $incentive['statusName'],
+                        'types' => [
+                                'Auto Show Cash' => ['cash', 'finance'],
+                                'Bonus Cash' => ['cash', 'finance'],
+                                'Cash Back' => ['cash', 'finance'],
+                                'Credit Card Rebate' => ['cash', 'finance'],
+                                'Cash on MSRP ' => ['cash', 'finance'],
+                                'Cash on Term APR' => ['finance'],
+                            ][$incentive['typeName']] ?? [],
+                    ];
+                })->filter(function ($incentive) {
+                    return ! empty($incentive['types']);
+                })->values();
 
-    public function getBestRebateIds(Client $Client)
-    {
-        $this->validate(request(), [
-            'category' => 'required|string|in:cash,finance,lease',
-            'zipcode' => 'required|string',
-            'vin' => 'required|string',
-        ]);
-
-        $vin = request('vin');
-        $zipcode = request('zipcode');
-
-        $version = Deal::where('vin', $vin)->with('versions')->firstOrFail()->versions->first();
-        $vehicleId = $version->jato_vehicle_id;
-
-        $incentives = /**
-         * @return \Illuminate\Support\Collection
-         */
-        (function () use ($Client, $vehicleId, $zipcode) {
-            switch (request('category')) {
-                case 'cash':
-                    return $Client->bestCashIncentivesByVehicleIdAndZipcode($vehicleId, $zipcode);
-                case 'finance':
-                    return $Client->bestFinanceIncentivesByVehicleIdAndZipcode($vehicleId, $zipcode);
-                case 'lease':
-                    return $Client->bestLeaseIncentivesByVehicleIdAndZipcode($vehicleId, $zipcode);
-                default:
-                    return [];
-            }
-        })();
-
-        $bestRebateIds = collect($incentives)
-            ->map(function ($incentive) {
-                return $incentive['subPrgId'];
-            })->values();
-
-        return response()->json([
-            'ids' => $bestRebateIds,
-        ]);
+            return response()->json([
+                'rebates' => $availableRebates,
+            ]);
+        });
     }
 }
