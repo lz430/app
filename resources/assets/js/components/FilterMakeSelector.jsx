3657c0631d9a98eecba33c8b14d171c126b63c70
Logan Henson <logan@loganhenson.com>
2017-09-13 16:02:10 -0500

    Update the left rail accordion to open and close sections according to the logic Rick gave us in the Google doc of questions

--- resources/assets/js/components/FilterMakeSelector.jsx
+++ resources/assets/js/components/FilterMakeSelector.jsx
@@ -8,30 +8,37 @@ class FilterMakeSelector extends React.PureComponent {
     render() {
         return (
             <div className="filter-selector">
-                {this.props.makes.map((make, index) => {
-                    return (
-                        <div
-                            key={index}
-                            className="filter-selector__selector"
-                            onClick={this.props.onSelectMake.bind(
-                                null,
-                                make.id
-                            )}
-                        >
-                            {R.contains(make.id, this.props.selectedMakes) ? (
-                                <SVGInline
-                                    width="15px"
-                                    height="15px"
-                                    className="filter-selector__checkbox filter-selector__checkbox--selected"
-                                    svg={zondicons['checkmark']}
-                                />
-                            ) : (
-                                <div className="filter-selector__checkbox" />
-                            )}
-                            {make.attributes.name}
-                        </div>
-                    );
-                })}
+                {this.props.makes ? (
+                    this.props.makes.map((make, index) => {
+                        return (
+                            <div
+                                key={index}
+                                className="filter-selector__selector"
+                                onClick={this.props.onSelectMake.bind(
+                                    null,
+                                    make.id
+                                )}
+                            >
+                                {R.contains(
+                                    make.id,
+                                    this.props.selectedMakes
+                                ) ? (
+                                    <SVGInline
+                                        width="15px"
+                                        height="15px"
+                                        className="filter-selector__checkbox filter-selector__checkbox--selected"
+                                        svg={zondicons['checkmark']}
+                                    />
+                                ) : (
+                                    <div className="filter-selector__checkbox" />
+                                )}
+                                {make.attributes.name}
+                            </div>
+                        );
+                    })
+                ) : (
+                    'Loading...'
+                )}
             </div>
         );
     }
@@ -43,7 +50,7 @@ FilterMakeSelector.propTypes = {
             name: PropTypes.string,
             logo: PropTypes.string,
         })
-    ).isRequired,
+    ),
     selectedMakes: PropTypes.arrayOf(PropTypes.string),
     onSelectMake: PropTypes.func.isRequired,
 };
