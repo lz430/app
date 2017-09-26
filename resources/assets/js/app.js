c08ca1a3d305a71267beb005212db37e852723bc
Samantha Geitz <samanthamichele7@gmail.com>
2017-09-26 11:25:15 -0500

    Formatting consistency

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -28,7 +28,8 @@ Array.from(document.getElementsByTagName('Financing')).map(element => {
         <Financing
             featuredPhoto={DeliverMyRide.featuredPhoto}
             purchase={DeliverMyRide.purchase}
-            user={DeliverMyRide.user} />,
+            user={DeliverMyRide.user} 
+        />,
         element);
 });
 
