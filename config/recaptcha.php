b250d1f40b53b114acb019842a0993e0419cbc7a
Cenitoria Melton <cenitoria@tighten.co>
2017-11-17 14:07:24 -0600

    Add Captcha
    DMR-147:https://maddogtechnology.atlassian.net/browse/DMR-147
    
    Added captcha v2 to the information request page.

---
+++ config/recaptcha.php
@@ -0,0 +1,66 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Keys
+    |--------------------------------------------------------------------------
+    |
+    | Set the public and private API keys as provided by reCAPTCHA.
+    |
+    | In version 2 of reCAPTCHA, public_key is the Site key,
+    | and private_key is the Secret key.
+    |
+    */
+    'public_key'     => env('RECAPTCHA_PUBLIC_KEY', '6Lfy1jgUAAAAAJUcnkpdG6wh2Zv5tHcM35idP7cA'),
+    'private_key'    => env('RECAPTCHA_PRIVATE_KEY', '6Lfy1jgUAAAAAGbTLSqyjwKmeOl7v7Aw6k1Fo5L4'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Template
+    |--------------------------------------------------------------------------
+    |
+    | Set a template to use if you don't want to use the standard one.
+    |
+    */
+    'template'    => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Driver
+    |--------------------------------------------------------------------------
+    |
+    | Determine how to call out to get response; values are 'curl' or 'native'.
+    | Only applies to v2.
+    |
+    */
+    'driver'      => 'curl',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Options
+    |--------------------------------------------------------------------------
+    |
+    | Various options for the driver
+    |
+    */
+    'options'     => [
+
+        'curl_timeout' => 1,
+        'curl_verify' => true,
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Version
+    |--------------------------------------------------------------------------
+    |
+    | Set which version of ReCaptcha to use.
+    |
+    */
+
+    'version'     => 2,
+
+];
