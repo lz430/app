c92ff65963bdbba20543451063e4b7792a88e5f9
Jose Soto <josecanhelp@gmail.com>
2018-01-10 22:53:00 -0500

    Dismiss info modal when "Get Rebates" is clicked

--- public/js/app.js
+++ public/js/app.js
@@ -57598,11 +57598,9 @@ var InfoModal = function (_React$PureComponent) {
 
             return _react2.default.createElement(
                 'div',
-                { className: 'modal',
-                    onClick: function onClick(e) {
+                { className: 'modal', onClick: function onClick(e) {
                         return _this2.closeIfOverlayClick(e);
-                    }
-                },
+                    } },
                 _react2.default.createElement('div', { className: 'modal__overlay' }),
                 _react2.default.createElement(
                     'div',
@@ -57610,7 +57608,12 @@ var InfoModal = function (_React$PureComponent) {
                     _react2.default.createElement(
                         'div',
                         { className: 'modal__content' },
-                        _react2.default.createElement(_InfoModalData2.default, { deal: this.props.deal })
+                        _react2.default.createElement(_InfoModalData2.default, {
+                            closeModal: function closeModal() {
+                                return _this2.toggleModal();
+                            },
+                            deal: this.props.deal
+                        })
                     )
                 )
             );
@@ -57641,12 +57644,16 @@ var InfoModal = function (_React$PureComponent) {
                     'a',
                     {
                         onClick: function onClick() {
-                            _this3.toggleModal();
+                            return _this3.toggleModal();
                         },
                         href: '#',
                         className: 'infomodal__button'
                     },
-                    _react2.default.createElement(_reactSvgInline2.default, { width: '15px', fill: 'grey', svg: _zondicons2.default['information-outline'] })
+                    _react2.default.createElement(_reactSvgInline2.default, {
+                        width: '15px',
+                        fill: 'grey',
+                        svg: _zondicons2.default['information-outline']
+                    })
                 ),
                 this.state.toggled ? this.renderModal() : ''
             );
@@ -57852,7 +57859,9 @@ var InfoModalData = function (_React$PureComponent) {
                             'a',
                             {
                                 onClick: function onClick() {
-                                    return _this2.props.selectDeal(_this2.props.deal);
+                                    _this2.props.selectDeal(_this2.props.deal);
+
+                                    _this2.props.closeModal();
                                 },
                                 href: '#'
                             },
@@ -58088,7 +58097,8 @@ InfoModalData.propTypes = {
         model: _propTypes2.default.string.isRequired,
         id: _propTypes2.default.number.isRequired,
         vin: _propTypes2.default.string.isRequired
-    })
+    }),
+    closeModal: _propTypes2.default.func.isRequired
 };
 
 var mapStateToProps = function mapStateToProps(state) {
