9f63d8b7bd736dfb4d03bc6cc910054682dc3ac8
Jose Soto <josecanhelp@gmail.com>
2018-01-27 02:28:56 -0500

    Add reselect package for computed properties

---
+++ resources/assets/js/selectors/index.js
@@ -0,0 +1,119 @@
+import { createSelector } from 'reselect';
+import { selectDeal } from '../actions/index';
+import R from 'ramda';
+
+const selectedDeal = state => state.selectedDeal;
+const zipcode = state => state.zipcode;
+const deal = (state, props) => props.deal;
+const targetsAvailable = state => state.targetsAvailable;
+const targetsSelected = state => state.targetsSelected;
+const bestOffers = state => state.bestOffers;
+const paymentType = state => state.selectedTab;
+const targetDefaults = state => state.targetDefaults;
+
+// Generate the target key for a specific deal
+const dealTargetKey = createSelector(
+    [deal, zipcode],
+    (deal, zipcode) => {
+        if(!deal) {
+            return null;
+        }
+        const year = deal.year;
+        const make = deal.make;
+        const model = deal.model;
+        const series = deal.series;
+        return `${zipcode}-${year}-${make}-${model}-${series}`;
+    }
+);
+
+export const makeDealTargetKey = () => {
+    return dealTargetKey;
+};
+
+// Show me all available targets for a specific deal
+const dealTargetsAvailable = createSelector(
+    [dealTargetKey, targetsAvailable],
+    (dealTargetKey, targetsAvailable) => {
+        return R.prop(dealTargetKey, targetsAvailable) || [];
+    }
+);
+
+export const makeDealTargetsAvailable = () => {
+    return dealTargetsAvailable;
+}
+
+// Show me all selected targets for a specific deal
+const dealTargetsSelected = createSelector(
+    [dealTargetKey, targetsSelected],
+    (dealTargetKey, targetsSelected) => {
+        return R.prop(dealTargetKey, targetsSelected) || [];
+    }
+)
+
+export const makeDealTargetsSelected = () => {
+    return dealTargetsSelected;
+}
+
+// Generate a string of unique target ids joined by '-'
+// This will be used to cache best offers on the front-end
+const selectedTargetsString = createSelector(
+    [dealTargetsSelected, targetDefaults],
+    (dealTargetsSelected, targetDefaults) => {
+        const selectedTargetIds = R.map(R.prop('targetId'), dealTargetsSelected) || [];
+        const uniqueSelectedTargetIds = R.uniq(R.concat(targetDefaults, selectedTargetIds));
+        return R.sort((a, b) => {
+            return a - b;
+        }, uniqueSelectedTargetIds).join('-');
+    }
+)
+
+// Generate the best offer key for a specific deal
+const dealBestOfferKey = createSelector(
+    [deal, zipcode, paymentType, selectedTargetsString],
+    (deal, zipcode, paymentType, selectedTargetsString) => {
+        return `${deal.id}-${zipcode}-${paymentType}-${selectedTargetsString}`;
+    }
+);
+
+export const makeDealBestOfferKey = () => {
+    return dealBestOfferKey;
+}
+
+// Show me the best offer for a specific deal or default to no best offer
+const dealBestOffer = createSelector(
+    [bestOffers, dealBestOfferKey],
+    (bestOffers, dealBestOfferKey) => {
+        return R.prop(dealBestOfferKey,  bestOffers) || {totalValue: 0, programs: []};
+    }
+)
+
+export const makeDealBestOffer = () => {
+    return dealBestOffer;
+}
+
+// Get the total value of the best offer for the deal
+const dealBestOfferTotalValue = createSelector(
+    [dealBestOffer],
+    (dealBestOffer) => {
+        return R.prop('totalValue', dealBestOffer);
+    }
+)
+
+export const makeDealBestOfferTotalValue = () => {
+    return dealBestOfferTotalValue;
+}
+
+// Generate the target key for globally the selected deal
+export const getSelectedTargetsKeyForSelectedDeal = createSelector(
+    [selectedDeal, zipcode],
+    (selectedDeal, zipcode) => {
+        if(!selectedDeal) {
+            return null;
+        }
+        const year = selectedDeal.year;
+        const make = selectedDeal.make;
+        const model = selectedDeal.model;
+        const series = selectedDeal.series;
+        return `${zipcode}-${year}-${make}-${model}-${series}`;
+    }
+)
