566a361794a9da8acf48fea63d338c68110300b5
Matt Stauffer <matt@tighten.co>
2018-02-20 20:53:05 -0500

    Make log level impact jato log

--- app/JATO/Log.php
+++ app/JATO/Log.php
@@ -8,10 +8,18 @@ use function storage_path;
 class Log
 {
     protected $filename;
+    protected $log_level;
+
+    protected $logLevels = [
+        'error',
+        'info',
+        'debug',
+    ];
 
     public function __construct()
     {
         $this->filename = storage_path('logs/jato.log');
+        $this->log_level = config('app.log_level');
     }
 
     public function debug($message, $details = [])
@@ -31,9 +39,18 @@ class Log
 
     public function log($level, $message, $details)
     {
+        if (! $this->shouldLog($level)) {
+            return;
+        }
+
         $date = date('[Y-m-d h:i:s] ');
         $level = strtoupper($level) . ': ';
         $details = empty($details) ? '' : ' | ' . print_r($details, true);
         File::append($this->filename, $date . $level . $message . $details . "\n");
+    }
+
+    private function shouldLog($level)
+    {
+        return array_search($level, $this->logLevels) <= array_search($this->log_level, $this->logLevels);
     }
 }
