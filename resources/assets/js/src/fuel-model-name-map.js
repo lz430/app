facccfdab3b76583a6199419664bfd8abd44ef9a
Matt Stauffer <matt@tighten.co>
2018-03-16 12:21:24 -0400

    Lower case a in allroad

--- resources/assets/js/src/fuel-model-name-map.js
+++ resources/assets/js/src/fuel-model-name-map.js
@@ -2,7 +2,7 @@ const modelNameMap = {
     'A3 Sedan': 'A3',
     'A5 Coupe': 'A5',
     'A8': 'A8 L',
-    'Allroad': 'A4 allroad',
+    'allroad': 'A4 allroad',
     '6 Series Gran Turismo': '6-series',
     'ATS Sedan': 'ATS',
     'ATS-V Sedan': 'ATS-V',
