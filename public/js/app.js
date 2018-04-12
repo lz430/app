5ba3e1ad1c3dae41032d1739ac1286364bb13b6f
Beau Simensen <beau@dflydev.com>
2018-04-11 22:10:59 -0500

    Add tabs and view details to compare page.

--- public/js/app.js
+++ public/js/app.js
@@ -68197,6 +68197,10 @@ var _Pricing = __webpack_require__(985);
 
 var _Pricing2 = _interopRequireDefault(_Pricing);
 
+var _AccuPricingModal = __webpack_require__(373);
+
+var _AccuPricingModal2 = _interopRequireDefault(_AccuPricingModal);
+
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -68289,6 +68293,16 @@ var ComparePage = function (_React$PureComponent) {
                 _react2.default.createElement(
                     'div',
                     { className: 'deal__buttons' },
+                    _react2.default.createElement(
+                        'button',
+                        {
+                            className: 'deal__button deal__button--small',
+                            onClick: function onClick() {
+                                return window.location = '/deals/' + deal.id;
+                            }
+                        },
+                        'View Details'
+                    ),
                     _react2.default.createElement(
                         'button',
                         {
@@ -68714,9 +68728,71 @@ var ComparePage = function (_React$PureComponent) {
             return anyHaveFuelType || anyHaveTransmissionType || anyHaveFeatures;
         }
     }, {
+        key: 'renderAccuPricingCta',
+        value: function renderAccuPricingCta() {
+            return _react2.default.createElement(
+                'div',
+                null,
+                _react2.default.createElement(
+                    'div',
+                    { className: 'accupricing-cta accupricing-cta--horizontal' },
+                    _react2.default.createElement(
+                        'a',
+                        { onClick: this.props.showAccuPricingModal },
+                        _react2.default.createElement('img', { src: '/images/accupricing-logo.png', className: 'accupricing-cta__logo' })
+                    ),
+                    _react2.default.createElement(
+                        'p',
+                        { className: 'accupricing-cta__disclaimer' },
+                        '* Includes taxes, dealer fees and rebates.'
+                    )
+                )
+            );
+        }
+    }, {
+        key: 'renderSelectedTabButtons',
+        value: function renderSelectedTabButtons() {
+            var _this9 = this;
+
+            return _react2.default.createElement(
+                'div',
+                { className: 'button-group' },
+                _react2.default.createElement(
+                    'div',
+                    {
+                        onClick: function onClick() {
+                            _this9.handleTabChange('cash');
+                        },
+                        className: 'button-group__button ' + (this.props.selectedTab === 'cash' ? 'button-group__button--selected' : '')
+                    },
+                    'Cash'
+                ),
+                _react2.default.createElement(
+                    'div',
+                    {
+                        onClick: function onClick() {
+                            _this9.handleTabChange('finance');
+                        },
+                        className: 'button-group__button ' + (this.props.selectedTab === 'finance' ? 'button-group__button--selected' : '')
+                    },
+                    'Finance'
+                ),
+                _react2.default.createElement(
+                    'div',
+                    {
+                        onClick: function onClick() {
+                            _this9.handleTabChange('lease');
+                        },
+                        className: 'button-group__button ' + (this.props.selectedTab === 'lease' ? 'button-group__button--selected' : '')
+                    },
+                    'Lease'
+                )
+            );
+        }
+    }, {
         key: 'render',
         value: function render() {
-            var _this9 = this;
+            var _this10 = this;
 
             return _react2.default.createElement(
                 'div',
@@ -68726,6 +68802,20 @@ var ComparePage = function (_React$PureComponent) {
                     { className: 'compare-page__body' },
                     _react2.default.createElement(
                         'div',
+                        { className: 'compare-page__top-row' },
+                        _react2.default.createElement(
+                            'div',
+                            { className: 'compare-page__top-row__section compare-page__top-row__section--accuPricing' },
+                            this.renderAccuPricingCta()
+                        ),
+                        _react2.default.createElement(
+                            'div',
+                            { className: 'compare-page__top-row__section compare-page__top-row__section--tabButtons' },
+                            this.renderSelectedTabButtons()
+                        )
+                    ),
+                    _react2.default.createElement(
+                        'div',
                         { className: 'compare-page-deals' },
                         this.props.deals.map(this.renderDeal)
                     ),
@@ -68733,14 +68823,14 @@ var ComparePage = function (_React$PureComponent) {
                         _AccordionTable2.default,
                         null,
                         function () {
-                            return _this9.renderSelectionsTable(_this9.props.compareList);
+                            return _this10.renderSelectionsTable(_this10.props.compareList);
                         }
                     ) : '',
                     _react2.default.createElement(
                         _AccordionTable2.default,
                         null,
                         function () {
-                            return _this9.renderWarrantyTable(_this9.props.compareList);
+                            return _this10.renderWarrantyTable(_this10.props.compareList);
                         }
                     ),
                     this.props.compareList.length ? this.renderFeaturesTable(this.props.compareList) : '',
@@ -68748,12 +68838,19 @@ var ComparePage = function (_React$PureComponent) {
                         _AccordionTable2.default,
                         null,
                         function () {
-                            return _this9.renderOptionalFeaturesTable(_this9.props.compareList);
+                            return _this10.renderOptionalFeaturesTable(_this10.props.compareList);
                         }
                     )
                 ),
-                this.props.selectedDeal ? this.renderCalculatorModal() : ''
+                this.props.selectedDeal ? this.renderCalculatorModal() : '',
+                _react2.default.createElement(_AccuPricingModal2.default, null)
             );
+        }
+    }, {
+        key: 'handleTabChange',
+        value: function handleTabChange(tabName) {
+            this.props.selectTab(tabName);
+            this.props.getBestOffersForLoadedDeals();
         }
     }]);
 
--- resources/assets/js/containers/ComparePage.jsx
+++ resources/assets/js/containers/ComparePage.jsx
@@ -16,6 +16,7 @@ import api from 'src/api';
 import miscicons from 'miscicons';
 import toTitleCase from 'titlecase';
 import Pricing from 'components/ComparePage/Pricing';
+import AccuPricingModal from 'components/AccuPricingModal';
 
 class ComparePage extends React.PureComponent {
     constructor(props) {
@@ -101,6 +102,14 @@ class ComparePage extends React.PureComponent {
         return (
             <Deal deal={deal} key={index}>
                 <div className="deal__buttons">
+                    <button
+                        className="deal__button deal__button--small"
+                        onClick={() =>
+                            (window.location = `/deals/${deal.id}`)
+                        }
+                    >
+                        View Details
+                    </button>
                     <button
                         className="deal__button deal__button--small deal__button--pink"
                         onClick={() =>
@@ -593,10 +602,77 @@ class ComparePage extends React.PureComponent {
         return anyHaveFuelType || anyHaveTransmissionType || anyHaveFeatures;
     }
 
+    renderAccuPricingCta() {
+        return (
+            <div>
+                <div className="accupricing-cta accupricing-cta--horizontal">
+                    <a onClick={this.props.showAccuPricingModal}>
+                        <img src="/images/accupricing-logo.png" className="accupricing-cta__logo" />
+                    </a>
+                    <p className="accupricing-cta__disclaimer">
+                        * Includes taxes, dealer fees and rebates.
+                    </p>
+                </div>
+            </div>
+        );
+    }
+
+    renderSelectedTabButtons() {
+        return (
+            <div className="button-group">
+                <div
+                    onClick={() => {
+                        this.handleTabChange('cash');
+                    }}
+                    className={`button-group__button ${
+                        this.props.selectedTab === 'cash'
+                            ? 'button-group__button--selected'
+                            : ''
+                        }`}
+                >
+                    Cash
+                </div>
+                <div
+                    onClick={() => {
+                        this.handleTabChange('finance');
+                    }}
+                    className={`button-group__button ${
+                        this.props.selectedTab === 'finance'
+                            ? 'button-group__button--selected'
+                            : ''
+                        }`}
+                >
+                    Finance
+                </div>
+                <div
+                    onClick={() => {
+                        this.handleTabChange('lease');
+                    }}
+                    className={`button-group__button ${
+                        this.props.selectedTab === 'lease'
+                            ? 'button-group__button--selected'
+                            : ''
+                        }`}
+                >
+                    Lease
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
             <div className="compare-page">
                 <div className="compare-page__body">
+                    <div className="compare-page__top-row">
+                        <div className="compare-page__top-row__section compare-page__top-row__section--accuPricing">
+                            {this.renderAccuPricingCta()}
+                        </div>
+                        <div className="compare-page__top-row__section compare-page__top-row__section--tabButtons">
+                            {this.renderSelectedTabButtons()}
+                        </div>
+                    </div>
+
                     <div className="compare-page-deals">
                         {this.props.deals.map(this.renderDeal)}
                     </div>
@@ -634,8 +710,14 @@ class ComparePage extends React.PureComponent {
                 </div>
 
                 {this.props.selectedDeal ? this.renderCalculatorModal() : ''}
+                <AccuPricingModal />
             </div>
         );
+    }
+
+    handleTabChange(tabName) {
+        this.props.selectTab(tabName);
+        this.props.getBestOffersForLoadedDeals();
     }
 }
 
