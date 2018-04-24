78b205f8bf04771f7e3f7a9f70f3cb5551506eb2
Eric Krawczyk <eric.krawczyk@gmail.com>
2018-04-23 21:21:04 -0400

    Hide extraneous steps on mobile

--- resources/views/partials/steps.blade.php
+++ resources/views/partials/steps.blade.php
@@ -13,9 +13,19 @@
     $check = file_get_contents(resource_path("assets/svg/zondicons/checkmark.svg"))
     @endphp
     @foreach ($steps as $i => $step)
-        <div class="step {{ $current == $i + 1 ? 'step--active' : '' }}">
+        <div class="step {{ $current == $i + 1 ? 'step--active' : '' }} {{ $current == $i || $current == $i + 1 || $current == $i + 2 ? 'step--show-mobile' : '' }}">
             <div class="step__icon">{!! $current > ($i + 1) ? $check : $i + 1 !!}</div>
             <div class="step__label">{{ $step }}</div>
         </div>
     @endforeach
+    <script>
+        if (window.innerWidth < 576) {
+            const steps = document.getElementsByClassName('step');
+            Array.from(steps).forEach(element => {
+                if (!element.className.includes('step--show-mobile')) {
+                    element.remove();
+                }
+            })
+        }
+    </script>
 </div>
