314dbb63276a4cc56690edfa977710a452a8e77a
Matt Wisner <matt@detroitweb.company>
2018-06-01 09:59:08 -0400

    PHP fuel API w/ tests

---
+++ tests/External/Fuel/ProductServiceTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\External\Fuel;
+
+
+use Tests\TestCase;
+use DeliverMyRide\Fuel\FuelClient;
+
+class ProductServiceTest extends TestCase
+{
+    /**
+     * Client factory
+     * @return FuelClient
+     */
+    public function getClient() : FuelClient {
+        return new FuelClient(
+            config('services.fuel.api_key')
+        );
+    }
+
+    /** @test **/
+    public function it_can_get_product_data() {
+        $client = $this->getClient();
+        $response = $client->product->list();
+        $this->assertGreaterThan(1, count($response));
+    }
+}
