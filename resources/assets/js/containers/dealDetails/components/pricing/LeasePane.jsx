e37dcd8a9c19d5c599a6b6fc43026cf5d0a0b2b4
Beau Simensen <beau@dflydev.com>
2018-06-23 23:27:10 -0500

    Add interactive bits to lease tab on deal detail
    Annual mileage is now selectable and the new term/monthly payment matrix is
    active. It works based on assuming no cash due.

--- resources/assets/js/containers/dealDetails/components/pricing/LeasePane.jsx
+++ resources/assets/js/containers/dealDetails/components/pricing/LeasePane.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import Discount from "./Discount";
-import Rebates from "../../containers/pricing/rebates/Rebates";
-import Line from "./Line";
-import Label from "./Label";
-import Value from "./Value";
+import Discount from './Discount';
+import Rebates from '../../containers/pricing/rebates/Rebates';
+import Line from './Line';
+import Label from './Label';
+import Value from './Value';
 
-class LeasePane extends React.PureComponent {
+export default class LeasePane extends React.PureComponent {
+    static defaultProps = {
+        onDiscountChange: (discountType, make = null) => {},
+        onRebatesChange: () => {},
+        onTermChange: term => {},
+        onAnnualMileageChange: annualMileage => {},
+        onCashDueChange: () => {},
+        onChange: () => {},
+    };
+
     render() {
-        const {dealPricing, onDiscountChange, onRebatesChange} = this.props;
+        const { dealPricing, onDiscountChange, onRebatesChange } = this.props;
 
         return (
             <div>
@@ -15,18 +24,20 @@ class LeasePane extends React.PureComponent {
                     <Label>MSRP</Label>
                     <Value>{dealPricing.msrp()}</Value>
                 </Line>
-                <Discount {...{dealPricing}} onChange={onDiscountChange} />
+                <Discount {...{ dealPricing }} onChange={onDiscountChange} />
                 <Line>
                     <Label>Selling Price</Label>
                     <Value>{dealPricing.baseSellingPrice()}</Value>
                 </Line>
-                {dealPricing.bestOfferValue() > 0 &&
-                <Line>
-                    <Label>Rebates Applied</Label>
-                    <Value isNegative={true}>{dealPricing.bestOffer()}</Value>
-                </Line>
-                }
-                <Rebates {...{dealPricing}} onChange={onRebatesChange}/>
+                {dealPricing.bestOfferValue() > 0 && (
+                    <Line>
+                        <Label>Rebates Applied</Label>
+                        <Value isNegative={true}>
+                            {dealPricing.bestOffer()}
+                        </Value>
+                    </Line>
+                )}
+                <Rebates {...{ dealPricing }} onChange={onRebatesChange} />
                 <Line>
                     <Label>Total Price</Label>
                     <Value>{dealPricing.yourPrice()}*</Value>
@@ -34,28 +45,136 @@ class LeasePane extends React.PureComponent {
                 <hr />
                 <Line>
                     <Label>Annual Miles</Label>
-                    <Value>{dealPricing.leaseAnnualMileage()}</Value>
+                    <Value>
+                        <select
+                            value={this.props.dealPricing.leaseAnnualMileageValue()}
+                            onChange={this.handleAnnualMileageChange}
+                        >
+                            {this.props.dealPricing
+                                .leaseAnnualMileageAvailable()
+                                .map((annualMileage, annualMileageIndex) => {
+                                    return (
+                                        <option
+                                            key={annualMileageIndex}
+                                            value={annualMileage}
+                                        >
+                                            {annualMileage}
+                                        </option>
+                                    );
+                                })}
+                        </select>
+                    </Value>
                 </Line>
-                <Line isSemiImportant={true}>
-                    <Label>Cash Due</Label>
-                    <Value>{dealPricing.leaseCashDue()}</Value>
+                <Line>
+                    <div className="cash-finance-lease-calculator__lease-table-container">
+                        <table className="cash-finance-lease-calculator__lease-table">
+                            <thead>
+                                <tr>
+                                    {/*<td className="cash-finance-lease-calculator__lease-table-cell--darker">
+                                        Cash Due
+                                    </td>*/}
+                                    {this.props.dealPricing.leaseTermsAvailable() &&
+                                        this.props.dealPricing
+                                            .leaseTermsAvailable()
+                                            .filter(term => {
+                                                return this.props.dealPricing.hasLeasePaymentsForTerm(
+                                                    term
+                                                );
+                                            })
+                                            .map((term, index) => {
+                                                return (
+                                                    <td
+                                                        className="cash-finance-lease-calculator__lease-table-cell--dark"
+                                                        key={index}
+                                                    >
+                                                        {term} Months
+                                                    </td>
+                                                );
+                                            })}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.props.dealPricing.leaseTermsAvailable() &&
+                                    this.props.dealPricing.leaseCashDueAvailable() &&
+                                    this.props.dealPricing
+                                        .leaseCashDueAvailable()
+                                        .map((cashDue, indexCashDue) => {
+                                            return (
+                                                <tr key={indexCashDue}>
+                                                    {/*<td className="cash-finance-lease-calculator__lease-table-cell--darker">
+                                            {util.moneyFormat(cashDue
+                                        )}</td>*/}
+                                                    {this.props.dealPricing
+                                                        .leaseTermsAvailable()
+                                                        .filter(term => {
+                                                            return this.props.dealPricing.hasLeasePaymentsForTerm(
+                                                                term
+                                                            );
+                                                        })
+                                                        .map(
+                                                            (
+                                                                term,
+                                                                termIndex
+                                                            ) => {
+                                                                let className = this.props.dealPricing.isSelectedLeasePaymentForTermAndCashDue(
+                                                                    term,
+                                                                    cashDue
+                                                                )
+                                                                    ? 'cash-finance-lease-calculator__lease-table-cell--selected'
+                                                                    : 'cash-finance-lease-calculator__lease-table-cell--selectable';
+
+                                                                return (
+                                                                    <td
+                                                                        className={
+                                                                            className
+                                                                        }
+                                                                        key={
+                                                                            termIndex
+                                                                        }
+                                                                        onClick={() =>
+                                                                            this.props.onChange(
+                                                                                this.props.dealPricing.leaseAnnualMileageValue(),
+                                                                                term,
+                                                                                cashDue
+                                                                            )
+                                                                        }
+                                                                    >
+                                                                        {this.props.dealPricing.leasePaymentsForTermAndCashDue(
+                                                                            term,
+                                                                            cashDue
+                                                                        )}
+                                                                    </td>
+                                                                );
+                                                            }
+                                                        )}
+                                                </tr>
+                                            );
+                                        })}
+                            </tbody>
+                        </table>
+                    </div>
                 </Line>
                 <Line>
                     <Label>Term Length</Label>
-                    <Value>{dealPricing.financeTerm()}</Value>
+                    <Value>{dealPricing.leaseTerm()}</Value>
                 </Line>
                 <Line isImportant={true}>
                     <Label>Monthly Payment</Label>
                     <Value>{dealPricing.monthlyPayments()}*</Value>
                 </Line>
             </div>
-        )
+        );
     }
+
+    handleTermChange = e => {
+        this.props.onTermChange(Number(e.target.value));
+    };
+
+    handleAnnualMileageChange = e => {
+        this.props.onAnnualMileageChange(Number(e.target.value));
+    };
+
+    handleCashDueChange = e => {
+        this.props.onCashDueChange(Number(e.target.value));
+    };
 }
-
-LeasePane.defaultProps = {
-    onDiscountChange: (discountType, make = null) => {},
-    onRebatesChange: () => {}
-};
-
-export default LeasePane;
