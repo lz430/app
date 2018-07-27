e540a63dd8e67c1a467b6335832f713b37ee2bb7
Matt Wisner <matt@detroitweb.company>
2018-07-27 14:01:45 -0400

    Make modal

--- resources/assets/js/pages/deal-list/Container.jsx
+++ resources/assets/js/pages/deal-list/Container.jsx
@@ -6,31 +6,38 @@ import Loading from 'icons/miscicons/Loading';
 import { StickyContainer } from 'react-sticky';
 import util from 'src/util';
 
-import Modal from 'components/Modal';
-
 import { getUserLocation } from 'apps/user/selectors';
 import { getIsPageLoading } from 'apps/page/selectors';
 
 import Deals from './components/Deals';
-import MakeSelector from './components/MakeSelector';
 import ToolbarSelectedFilters from './components/ToolbarSelectedFilters';
 import ToolbarPrice from './components/ToolbarPrice';
 import FilterPanel from './components/FilterPanel';
 import NoDealsOutOfRange from './components/NoDealsOutOfRange';
+import ModalMakeSelector from './components/ModalMakeSelector';
 
-import { initDealListData, closeMakeSelectorModal } from './actions';
+import {
+    initDealListData,
+    closeMakeSelectorModal,
+    toggleSearchFilter,
+} from './actions';
 import { getSelectedFiltersByCategory } from './selectors';
+import { filterItemType } from 'types';
+import classNames from 'classnames';
 
 class Container extends React.PureComponent {
     static propTypes = {
         searchQuery: PropTypes.object.isRequired,
-        onInit: PropTypes.func.isRequired,
-        onCloseMakeSelectorModal: PropTypes.func.isRequired,
         makeSelectorModalIsOpen: PropTypes.bool,
         smallFiltersShown: PropTypes.bool,
         selectedFiltersByCategory: PropTypes.object,
         userLocation: PropTypes.object.isRequired,
         isLoading: PropTypes.bool,
+        makes: PropTypes.arrayOf(filterItemType),
+        fallbackLogoImage: PropTypes.string.isRequired,
+        onInit: PropTypes.func.isRequired,
+        onToggleSearchFilter: PropTypes.func.isRequired,
+        onCloseMakeSelectorModal: PropTypes.func.isRequired,
     };
 
     componentDidMount() {
@@ -43,26 +50,30 @@ class Container extends React.PureComponent {
 
     renderMakeSelectionModal() {
         return (
-            <Modal
-                onClose={this.props.onCloseMakeSelectorModal}
-                title="SELECT ONE OR MORE BRANDS TO COMPARE"
-                closeText="Show available vehicles"
-                buttonCloseDisabled={false}
-            >
-                <MakeSelector />
-            </Modal>
+            <ModalMakeSelector
+                toggle={this.props.onCloseMakeSelectorModal}
+                isOpen={this.props.makeSelectorModalIsOpen}
+                selectedFiltersByCategory={this.props.selectedFiltersByCategory}
+                makes={this.props.makes}
+                onToggleSearchFilter={this.props.onToggleSearchFilter}
+                fallbackLogoImage={this.props.fallbackLogoImage}
+            />
         );
     }
 
     renderFilterPanel() {
-        const className =
-            'filter-page__filter-panel ' +
-            (util.windowIsLargerThanSmall(this.props.window.width)
-                ? ''
-                : 'filter-page__filter-panel--small ' +
-                  (this.props.smallFiltersShown
-                      ? 'filter-page__filter-panel--small-filters-shown'
-                      : 'filter-page__filter-panel--small-filters-hidden'));
+        const className = classNames({
+            'filter-page__filter-panel': true,
+            'filter-page__filter-panel--small': !util.windowIsLargerThanSmall(
+                this.props.window.width
+            ),
+            'filter-page__filter-panel--small-filters-shown':
+                !util.windowIsLargerThanSmall(this.props.window.width) &&
+                this.props.smallFiltersShown,
+            'filter-page__filter-panel--small-filters-hidden':
+                !util.windowIsLargerThanSmall(this.props.window.width) &&
+                !this.props.smallFiltersShown,
+        });
 
         return (
             <div className={className}>
@@ -72,14 +83,18 @@ class Container extends React.PureComponent {
     }
 
     renderDeals() {
-        const className =
-            'filter-page__deals ' +
-            (util.windowIsLargerThanSmall(this.props.window.width)
-                ? ''
-                : 'filter-page__deals--small ' +
-                  (this.props.smallFiltersShown
-                      ? 'filter-page__deals--small-filters-shown'
-                      : 'filter-page__deals--small-filters-hidden'));
+        const className = classNames({
+            'filter-page__deals': true,
+            'filter-page__deals--small': !util.windowIsLargerThanSmall(
+                this.props.window.width
+            ),
+            'filter-page__deals--small-filters-shown':
+                !util.windowIsLargerThanSmall(this.props.window.width) &&
+                this.props.smallFiltersShown,
+            'filter-page__deals--small-filters-hidden':
+                !util.windowIsLargerThanSmall(this.props.window.width) &&
+                !this.props.smallFiltersShown,
+        });
 
         return (
             <div className={className}>
@@ -114,10 +129,7 @@ class Container extends React.PureComponent {
 
         return (
             <StickyContainer>
-                {this.props.makeSelectorModalIsOpen
-                    ? this.renderMakeSelectionModal()
-                    : ''}
-
+                {this.renderMakeSelectionModal()}
                 {this.renderFilterPanelAndDeals()}
             </StickyContainer>
         );
@@ -132,6 +144,8 @@ const mapStateToProps = state => {
         searchQuery: state.pages.dealList.searchQuery,
         userLocation: getUserLocation(state),
         selectedFiltersByCategory: getSelectedFiltersByCategory(state),
+        makes: state.pages.dealList.filters.make,
+        fallbackLogoImage: state.common.fallbackLogoImage,
         isLoading: getIsPageLoading(state),
     };
 };
@@ -143,6 +157,9 @@ const mapDispatchToProps = dispatch => {
         },
         onCloseMakeSelectorModal: () => {
             return dispatch(closeMakeSelectorModal());
+        },
+        onToggleSearchFilter: item => {
+            return dispatch(toggleSearchFilter('make', item));
         },
     };
 };
