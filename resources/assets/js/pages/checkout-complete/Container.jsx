18f7e211d9ac37f0ff94e96d8cbb487d9836e780
Beau Simensen <beau@dflydev.com>
2018-08-07 20:20:33 -0500

    Standardize deal colors.

--- resources/assets/js/pages/checkout-complete/Container.jsx
+++ resources/assets/js/pages/checkout-complete/Container.jsx
@@ -19,6 +19,7 @@ import FinanceDetails from 'components/checkout/FinanceDetails';
 import LeaseDetails from 'components/checkout/LeaseDetails';
 import CashDetails from 'components/checkout/CashDetails';
 import InvalidCheckoutPage from 'components/checkout/InvalidCheckoutPage';
+import DealColors from '../../components/Deals/DealColors';
 
 class CheckoutCompleteContainer extends React.PureComponent {
     static propTypes = {
@@ -59,7 +60,7 @@ class CheckoutCompleteContainer extends React.PureComponent {
                                 {strings.dealModelTrim(deal)}
                             </div>
                             <div className="colors">
-                                {strings.dealColors(deal)}
+                                <DealColors deal={deal} />
                             </div>
                             <div className="stock-number">
                                 <DealStockNumber deal={deal} />
