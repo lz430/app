5da2e146f5efe9191d0e856cf73e1c794bbed218
Matt Wisner <matt@detroitweb.company>
2018-09-20 08:40:19 -0400

    Reduce via body type with body

--- src/RIS/Manager/VersionToVehicle.php
+++ src/RIS/Manager/VersionToVehicle.php
@@ -542,7 +542,6 @@ class VersionToVehicle
             return in_array($params['model'], $vehicle->filters->MODEL);
         });
 
-
         $vehicles = $this->filterUnlessNone($vehicles, 'filters', 'MODEL_CODE', $params['model_code']);
         $vehicles = $this->filterUnlessNone($vehicles, 'filters', 'PACKAGE_CODE', $params['model_code']);
 
@@ -559,6 +558,7 @@ class VersionToVehicle
         $vehicles = $this->filterUnlessNone($vehicles, 'filters', 'MODEL', $params['trim']);
         $vehicles = $this->filterUnlessNone($vehicles, 'filters', 'DRIVE_TYPE_CODE', [$params['driven_wheels']]);
         $vehicles = $this->filterUnlessNone($vehicles, 'filters', 'BODY_TYPE', [$params['cab']]);
+        $vehicles = $this->filterUnlessNone($vehicles, 'filters', 'BODY_TYPE', [$params['body']]);
         $vehicles = $this->filterUnlessNone($vehicles, 'filters', 'TRAN_TYPE', [$params['transmission']]);
 
         return collect($vehicles)->map(function ($item) {
