ef91cc77b32318388622d853619a3e1dbd7053dd
Chris Povinelli <cpovinelli@delivermyride.com>
2018-09-27 14:16:07 -0400

    Moved around the admin vauto access and updated some logic for the vauto admin area

--- app/Http/Controllers/Admin/VAutoDumpController.php
+++ app/Http/Controllers/Admin/VAutoDumpController.php
@@ -32,10 +32,14 @@ class VAutoDumpController extends Controller
 
     public function getFiles()
     {
-        $files = File::files(realpath(base_path('storage/app/public/importbackups')));
+        $files = File::isDirectory(realpath(base_path('storage/app/public/importbackups'))) ? File::files(realpath(base_path('storage/app/public/importbackups'))) : null;
         $data = [];
-        foreach($files as $file) {
-            $data[]['filename'] = $file->getFilename();
+        if(isset($files)) {
+            foreach($files as $file) {
+                $data[]['filename'] = $file->getFilename();
+            }
+        } else {
+            $data = null;
         }
         return view('admin.vauto-archives', ['dumps' => $data]);
     }
