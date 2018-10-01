183ebc369ce97f1094c052c4802721f069847f57
Matt Wisner <matt@detroitweb.company>
2018-10-01 15:09:36 -0400

    Fixing app naming

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
-import 'bootstrap'
-import 'App.scss'
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import configureStore, { history } from 'store'
+import 'bootstrap';
+import 'App.scss';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import configureStore, { history } from 'store';
 
-import DealList from 'pages/deal-list/Container'
-import DealDetail from 'pages/deal-detail/Container'
-import ComparePage from 'pages/compare/Container'
-import CheckoutConfirm from 'pages/checkout-confirm/Container'
-import CheckoutFinancing from 'pages/checkout-financing/Container'
-import CheckoutComplete from 'pages/checkout-complete/Container'
-import DeliverMyRide from 'components/App/App'
+import DealList from 'pages/deal-list/Container';
+import DealDetail from 'pages/deal-detail/Container';
+import ComparePage from 'pages/compare/Container';
+import CheckoutConfirm from 'pages/checkout-confirm/Container';
+import CheckoutFinancing from 'pages/checkout-financing/Container';
+import CheckoutComplete from 'pages/checkout-complete/Container';
+import DeliverMyRide from 'components/App/App';
 
-import { Route, Switch } from 'react-router-dom'
-import { ConnectedRouter } from 'connected-react-router'
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
 
-const { store, persistor } = configureStore()
+const { store, persistor } = configureStore();
 
 class App extends Component {
     render() {
@@ -52,8 +52,8 @@ class App extends Component {
                     </ConnectedRouter>
                 </PersistGate>
             </Provider>
-        )
+        );
     }
 }
 
-export default App
+export default App;
