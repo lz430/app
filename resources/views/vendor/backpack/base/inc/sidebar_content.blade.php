96a5bfa7ee46ec8a638da48ecd1f0ce7c34dd56d
Chris Povinelli <cpovinelli85@gmail.com>
2018-10-09 16:18:27 -0400

    Updated price rules admin layout

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -49,15 +49,15 @@
 
 <li class="header">Reports</li>
 <li>
-    <a href="/admin/reports/deals-without-rules">
-        <i class="fa fa-image"></i>
-        <span>Deals Missing Source Price</span>
-    </a>
-</li>
-<li>
     <a href="/admin/archived-dumps">
         <i class="fa fa-file"></i>
         <span>Archived Dumps</span>
+    </a>
+</li>
+<li>
+    <a href="/admin/reports/deals-without-rules">
+        <i class="fa fa-image"></i>
+        <span>Deals Missing Source Price</span>
     </a>
 </li>
 <li>
