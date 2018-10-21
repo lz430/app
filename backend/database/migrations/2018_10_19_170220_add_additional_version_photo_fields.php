f7c1721efce79ba455e2d317f5d0d2acb702e871
Matt Wisner <matt@detroitweb.company>
2018-10-21 17:28:15 -0400

    More fixes

--- backend/database/migrations/2018_10_19_170220_add_additional_version_photo_fields.php
+++ backend/database/migrations/2018_10_19_170220_add_additional_version_photo_fields.php
@@ -19,7 +19,7 @@ class AddAdditionalVersionPhotoFields extends Migration
             $table->string('color_simple')->nullable()->after('color');
             $table->string('color_rgb')->nullable()->after('color_simple');
             $table->string('color')->nullable()->change();
-            $table->unique('url');
+            $table->unique(['version_id', 'url']);
         });
     }
 
