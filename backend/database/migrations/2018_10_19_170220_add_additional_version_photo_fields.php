885f6007c30ec911e15653b0436c5675fb75b723
Matt Wisner <matt@detroitweb.company>
2018-10-22 13:19:48 -0400

    Truncate version photos during migration

--- backend/database/migrations/2018_10_19_170220_add_additional_version_photo_fields.php
+++ backend/database/migrations/2018_10_19_170220_add_additional_version_photo_fields.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Models\JATO\VersionPhoto;
 
 class AddAdditionalVersionPhotoFields extends Migration
 {
@@ -13,6 +14,8 @@ class AddAdditionalVersionPhotoFields extends Migration
      */
     public function up()
     {
+        VersionPhoto::truncate();
+
         Schema::table('versions_photos', function (Blueprint $table) {
             $table->string('type')->nullable()->after('url');
             $table->string('description')->nullable()->after('type');
