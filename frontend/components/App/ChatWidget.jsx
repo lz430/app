6d3ac599920f2b4e7b0cc4c2adb9975e8017af12
Matt Wisner <matt@detroitweb.company>
2018-10-23 10:03:19 -0400

    Replace livechat with hubspot

--- frontend/components/App/ChatWidget.jsx
+++ frontend/components/App/ChatWidget.jsx
@@ -4,9 +4,6 @@ import PropTypes from 'prop-types';
 import ChatBubbleDots from '../../icons/zondicons/chat-bubble-dots.svg';
 import classNames from 'classnames';
 
-import { ChatContext } from '../../core/contexts';
-import config from '../../core/config';
-
 class ChatWidget extends React.PureComponent {
     static propTypes = {
         presentation: PropTypes.string.isRequired,
@@ -16,63 +13,24 @@ class ChatWidget extends React.PureComponent {
         presentation: 'modal',
     };
 
-    renderChat(chatSettings) {
-        let label = 'Contact Us';
-        if (chatSettings.chatShow && chatSettings.chatAgents) {
-            label = 'Chat With Us';
-        } else if (chatSettings.chatShow) {
-            label = 'Get Help';
-        }
-
+    render() {
         if (this.props.presentation === 'modal') {
             return (
                 <div className={classNames('modal-widget', 'chat-button')}>
-                    {!chatSettings.chatShow && (
-                        <a
-                            className="btn btn-primary"
-                            href={config.MARKETING_URL + '/contact/'}
-                        >
-                            {label}
-                        </a>
-                    )}
-
-                    {chatSettings.chatShow && (
-                        <span
-                            className="btn btn-primary"
-                            onClick={() => chatSettings.onOpenChat()}
-                        >
-                            <span className="d-md-inline">{label}</span>
-                        </span>
-                    )}
+                    <a href="#hs-chat-open" className="btn btn-primary">
+                        <span className="d-md-inline">Chat With Us</span>
+                    </a>
                 </div>
             );
         }
 
-        if (!chatSettings.chatShow) {
-            return false;
-        }
-
         return (
-            <span
-                className={classNames('chat-button', {
-                    hidden: !chatSettings.chatShow,
-                })}
-            >
-                <span onClick={() => chatSettings.onOpenChat()}>
+            <span className={classNames('chat-button')}>
+                <a href="#hs-chat-open">
                     <ChatBubbleDots />
-                    <span>{label}</span>
-                </span>
+                    <span>Chat With Us</span>
+                </a>
             </span>
-        );
-    }
-
-    render() {
-        return (
-            <ChatContext.Consumer>
-                {chatSettings => {
-                    return this.renderChat(chatSettings);
-                }}
-            </ChatContext.Consumer>
         );
     }
 }
