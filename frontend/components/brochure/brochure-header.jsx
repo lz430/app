d3aef1e049f25512bb268cc349bc0f3383140c4b
Lenny <430designs@gmail.com>
2018-10-23 09:46:47 -0400

    Brochure desktop header

--- frontend/components/brochure/brochure-header.jsx
+++ frontend/components/brochure/brochure-header.jsx
@@ -24,52 +24,58 @@ export default class Header extends React.PureComponent {
 
     render() {
         return (
-            <Navbar expand="md">
-                <NavbarBrand href="/">
-                    <img
-                        alt="Deliver My Ride"
-                        src="/static/images/dmr-logo.svg"
-                    />
-                </NavbarBrand>
-                <NavbarToggler onClick={this.toggle} />
+            <header className="brochure-header">
+                <Navbar expand="md">
+                    <NavbarBrand href="/">
+                        <img
+                            alt="Deliver My Ride"
+                            src="/static/images/dmr-logo.svg"
+                        />
+                    </NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} className="" />
 
-                <div className="mr-auto" />
+                    <div className="mr-auto" />
 
-                <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav className="ml-auto" navbar>
-                        <NavItem>
-                            <Link
-                                href="/how-it-works"
-                                as="/brochure/how-it-works"
-                                passHref
-                            >
-                                <NavLink>Contact</NavLink>
-                            </Link>
-                        </NavItem>
-                        <NavItem>
-                            <Link href="/about" as="/brochure/about" passHref>
-                                <NavLink>About</NavLink>
-                            </Link>
-                        </NavItem>
-                        <NavItem>
-                            <Link
-                                href="/contact"
-                                as="/brochure/contact"
-                                passHref
-                            >
-                                <NavLink>Contact</NavLink>
-                            </Link>
-                        </NavItem>
-                        <NavItem>
-                            <Link href="/deal-list" as="/filter" passHref>
-                                <NavLink className="btn btn-primary">
-                                    Get Started
-                                </NavLink>
-                            </Link>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-            </Navbar>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <Link
+                                    href="/how-it-works"
+                                    as="/brochure/how-it-works"
+                                    passHref
+                                >
+                                    <NavLink>How It Works</NavLink>
+                                </Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link
+                                    href="/about"
+                                    as="/brochure/about"
+                                    passHref
+                                >
+                                    <NavLink>About</NavLink>
+                                </Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link
+                                    href="/contact"
+                                    as="/brochure/contact"
+                                    passHref
+                                >
+                                    <NavLink>Contact</NavLink>
+                                </Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link href="/deal-list" as="/filter" passHref>
+                                    <NavLink className="btn btn-primary">
+                                        Get Started
+                                    </NavLink>
+                                </Link>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </header>
         );
     }
 }
