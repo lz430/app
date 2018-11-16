50301b335c872db88fe854b8ae571bd3e46d8473
Lenny <430designs@gmail.com>
2018-11-16 11:05:21 -0500

    Removing isolating span on faq content

--- frontend/content/faqs.js
+++ frontend/content/faqs.js
@@ -27,7 +27,7 @@ export default [
         category: 'General Questions',
         title: 'Do you sell my data to third-parties?',
         content:
-            '<span>We are not a lead provider so we never sell your information, meaning you will never get calls from <u> anyone </u> trying to sell you a car. We simply are here to provide you with the information and direction you need during your car buying journey.</span>',
+            'We are not a lead provider so we never sell your information, meaning you will never get calls from <u> anyone </u> trying to sell you a car. We simply are here to provide you with the information and direction you need during your car buying journey.',
     },
     {
         category: 'General Questions',
