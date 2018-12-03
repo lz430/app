66a943ef6625326e5915624119dea82fbbd2b2e4
Matt Wisner <matt@detroitweb.company>
2018-12-02 21:18:15 -0500

    Adding makes and style images"
    "

--- frontend/content/styles.js
+++ frontend/content/styles.js
@@ -1,42 +1,50 @@
 export default [
     {
-        title: 'Sport Utility Vehicle',
+        title: 'Sport Utility Vehicles',
         value: 'suv',
         query: ['style:Sport Utility Vehicle'],
+        icon: '/static/brochure/styles/SUV_Grouping.jpg',
     },
     {
-        title: 'Sedan',
+        title: 'Sedans',
         value: 'sedan',
         query: ['style:Sedan'],
+        icon: '/static/brochure/styles/Sedan_Grouping.jpg',
     },
     {
-        title: 'Convertible',
+        title: 'Convertibles',
         value: 'convertible',
         query: ['style:Convertible'],
+        icon: '/static/brochure/styles/Sedan_Grouping.jpg',
     },
     {
-        title: 'Coupe',
+        title: 'Coupes',
         value: 'coupe',
         query: ['style:Coupe'],
+        icon: '/static/brochure/styles/Coupe_Grouping.jpg',
     },
     {
-        title: 'Hatchback',
+        title: 'Hatchbacks & Crossovers',
         value: 'hatchback',
         query: ['style:Hatchback'],
+        icon: '/static/brochure/styles/Hatchback_Grouping.jpg',
     },
     {
-        title: 'Pickup',
+        title: 'Pickups',
         value: 'pickup',
         query: ['style:Pickup'],
+        icon: '/static/brochure/styles/Pickup_Grouping.jpg',
     },
     {
-        title: 'Minivan',
+        title: 'Minivans',
         value: 'minivan',
         query: ['style:Minivan'],
+        icon: '/static/brochure/styles/VanMiniVan_Grouping.jpg',
     },
     {
-        title: 'Wagon',
+        title: 'Wagons',
         value: 'wagon',
         query: ['style:Wagon'],
+        icon: '/static/brochure/styles/Wagons_Grouping.jpg',
     },
 ];
