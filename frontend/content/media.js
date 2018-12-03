c5da3bb911346f792ae44b0f9edc2c29a24b4b7b
Matt Wisner <matt@detroitweb.company>
2018-12-03 09:22:34 -0500

    Adding featured in content

--- frontend/content/media.js
+++ frontend/content/media.js
@@ -2,8 +2,7 @@ export default [
     {
         name: 'Detroit Free Press',
         date: 'Jan. 12, 2017',
-        content: '',
-        logo: '../static/images/DetroitFreePress.png',
+        logo: '/static/brochure/featured/DetroitFreePress.png',
         link:
             'https://www.freep.com/story/money/business/michigan/2017/01/12/deliver-my-ride-buyers-car/96501240/',
     },
@@ -11,8 +10,32 @@ export default [
         name: "Crain's Detroit",
         date: 'Sept. 19, 2018',
         content: 'Deliver My Ride updates platform to drive growth',
-        logo: '../static/images/crainsDetroitBusiness.jpg',
+        logo: '/static/brochure/featured/crains.jpeg',
         link:
-            'https://www.freep.com/story/sponsor-story/top-workplaces-2018/2018/11/11/maddog-technology-epitomizes-michigans-tech-prowess-and-entrepreneurial-spirit/1929850002/',
+            'https://www.crainsdetroit.com/retail/deliver-my-ride-updates-platform-drive-growth',
+    },
+    {
+        name: 'TechCrunch',
+        date: 'Sept. 19, 2018',
+        content: 'Deliver My Ride updates platform to drive growth',
+        logo: '/static/brochure/featured/techcrunch_transparent.png',
+        link:
+            'https://techcrunch.com/2017/01/17/deliver-my-ride-delivers-another-car-buying-service/',
+    },
+    {
+        name: 'Fox 2 News',
+        date: 'Sept. 19, 2018',
+        content: 'Deliver My Ride updates platform to drive growth',
+        logo: '/static/brochure/featured/logo-fox-2-detroit-wjbk-alt.png',
+        link:
+            'https://www.fox2detroit.com/news/local-news/deliver-my-ride-allows-online-car-shopping-brought-to-your-door',
+    },
+    {
+        name: 'DBusiness',
+        date: 'Sept. 19, 2018',
+        content: 'Deliver My Ride updates platform to drive growth',
+        logo: '/static/brochure/featured/dbusiness.png',
+        link:
+            'https://www.dbusiness.com/daily-news/Annual-2018/Pontiacs-Deliver-My-Ride-Launches-Online-Car-buying-Platform/',
     },
 ];
