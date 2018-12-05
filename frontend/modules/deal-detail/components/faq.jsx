c805a0bf686299d95d2bbc52d3647a560bbea11f
Lenny <430designs@gmail.com>
2018-12-05 13:20:41 -0500

    Fixing circleCi errors

--- frontend/modules/deal-detail/components/faq.jsx
+++ frontend/modules/deal-detail/components/faq.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 
-import { Row, Col, Button, FormGroup, Alert } from 'reactstrap';
-
-import { faSpinner } from '@fortawesome/pro-light-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { track } from '../../../core/services';
 import Faqs from '../../../content/deal-faqs';
-import ApiClient from '../../../store/api';
 import FaqGroup from '../../../components/Deals/dealFaqGroup';
 
 class ListGroupCollapse extends React.Component {
