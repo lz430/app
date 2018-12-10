c0093d81226983e0d944a6ce26ceda15149c968f
Leonard <430designs@gmail.com>
2018-12-10 12:41:20 -0500

    Adding 1 FAQ open at a time funcitonality

--- frontend/modules/deal-detail/components/faq.jsx
+++ frontend/modules/deal-detail/components/faq.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
-import Faqs from '../../../content/deal-faqs';
+import Faqs from '../../../content/faqs';
 import FaqGroup from '../../../components/Deals/dealFaqGroup';
 
 class ListGroupCollapse extends React.Component {
     state = {
-        collapse: false,
-        active: false,
-        category: 'General Questions',
+        category: 'Deal FAQ',
+        activeFaqKey: false,
     };
+
+    toggleActiveFaq(faqKey) {
+        // If the user clicks on the open faq, close all faqss
+        if (faqKey === this.state.activeFaqKey) {
+            this.setState({
+                activeFaqKey: false,
+            });
+        } else {
+            this.setState({
+                activeFaqKey: faqKey,
+            });
+        }
+    }
 
     getFaqContent() {
         return Faqs.filter(item => item.category === this.state.category);
@@ -20,7 +32,12 @@ class ListGroupCollapse extends React.Component {
                 <h3> Frequently Asked Questions </h3>
                 <ul className="deal__faq__list">
                     {this.getFaqContent().map(item => (
-                        <FaqGroup key={item.title} item={item} />
+                        <FaqGroup
+                            key={item.title}
+                            item={item}
+                            isOpen={this.state.activeFaqKey === item.title}
+                            onToggle={this.toggleActiveFaq.bind(this)}
+                        />
                     ))}
                 </ul>
             </React.Fragment>
