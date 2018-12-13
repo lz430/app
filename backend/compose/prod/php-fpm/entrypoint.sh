071b768681973b9b961fcb1b0476fdfcfac21631
Matt Wisner <matt@detroitweb.company>
2018-12-13 10:15:28 -0500

    Fixing CLI command issues, scheduler, worker, etc

--- backend/compose/prod/php-fpm/entrypoint.sh
+++ backend/compose/prod/php-fpm/entrypoint.sh
@@ -2,21 +2,4 @@
 
 set -e
 
-role=${CONTAINER_ROLE:-app}
-
-echo ${role}
-
-if [ "$role" = "app" ]; then
-    service nginx start
-    php-fpm
-elif [ "$role" = "queue" ]; then
-    php /app/artisan queue:work --verbose --tries=3 --timeout=90
-elif [ "$role" = "scheduler" ]; then
-    while [ true ]
-    do
-      php /app/artisan schedule:run --verbose --no-interaction &
-      sleep 60
-    done
-else
-    exec "$@"
-fi
+exec "$@"
