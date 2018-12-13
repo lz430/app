54cfc3a89b679f9ff012a469bc70b728f8c163a1
Matt Wisner <matt@detroitweb.company>
2018-12-13 16:45:32 -0500

    Minor style changes, removing buttons that do not work

--- frontend/modules/deal-detail/components/Promise.jsx
+++ frontend/modules/deal-detail/components/Promise.jsx
@@ -9,13 +9,13 @@ export default class extends React.PureComponent {
                 <Col xs="12">
                     <h2 className="text-center"> We Promise </h2>
                 </Col>
-                <Row className="deal__promise__columns no-gutters">
+                <Row className="deal__promise__columns no-gutters bg-white shadow-sm">
                     <Col md="4">
                         <MediumAndUp>
                             <Row className="no-gutters">
                                 <Col md="1" sm="1" xs="1">
                                     <img
-                                        src="../../static/images/icons/promise-shield.png"
+                                        src="/static/images/icons/promise-shield.png"
                                         alt="Our promise shield"
                                     />
                                 </Col>
@@ -23,17 +23,16 @@ export default class extends React.PureComponent {
                                     <h5 className="text-uppercase">
                                         Not to sell you out{' '}
                                     </h5>
-                                    <p>
+                                    <p className="text-sm">
                                         We are not a lead generating site for
                                         dealerships. If anyone asks for your
                                         contact information without promising
-                                        that you won&apos;t be spammed &dash;
+                                        that you won&apos;t be spammed -
                                         you&apos;ll be spammed. At Deliver My
                                         Ride you control the process of
-                                        purchasing your new vehicle 100&percnt;
-                                        online, and your information stays
-                                        private, meaning no annoying sales calls
-                                        &dash; ever&excl;
+                                        purchasing your new vehicle 100% online,
+                                        and your information stays private,
+                                        meaning no annoying sales calls - ever
                                     </p>
                                 </Col>
                             </Row>
@@ -41,7 +40,7 @@ export default class extends React.PureComponent {
                         <SmallAndDown>
                             <Row className="no-gutters">
                                 <img
-                                    src="../../static/images/icons/promise-shield.png"
+                                    src="/static/images/icons/promise-shield.png"
                                     alt="Our promise shield"
                                 />
                                 <h5 className="text-uppercase">
@@ -50,17 +49,16 @@ export default class extends React.PureComponent {
                             </Row>
                             <Row>
                                 <Col>
-                                    <p>
+                                    <p className="text-sm">
                                         We are not a lead generating site for
                                         dealerships. If anyone asks for your
                                         contact information without promising
-                                        that you won&apos;t be spammed &dash;
+                                        that you won&apos;t be spammed -
                                         you&apos;ll be spammed. At Deliver My
                                         Ride you control the process of
-                                        purchasing your new vehicle 100&percnt;
-                                        online, and your information stays
-                                        private, meaning no annoying sales calls
-                                        &dash; ever&excl;
+                                        purchasing your new vehicle 100% online,
+                                        and your information stays private,
+                                        meaning no annoying sales calls - ever
                                     </p>
                                 </Col>
                             </Row>
@@ -72,7 +70,7 @@ export default class extends React.PureComponent {
                             <Row className="no-gutters">
                                 <Col md="1" sm="1" xs="1">
                                     <img
-                                        src="../../static/images/icons/promise-mag.png"
+                                        src="/static/images/icons/promise-mag.png"
                                         alt="Our promise mag"
                                     />
                                 </Col>
@@ -80,15 +78,15 @@ export default class extends React.PureComponent {
                                     <h5 className="text-uppercase">
                                         Not to try to fool you{' '}
                                     </h5>
-                                    <p>
+                                    <p className="text-sm">
                                         Are you a helicopter pilot, ski-patrol
                                         surgeon and recent college graduate that
-                                        happens to be an astronaut&quest; Yeah,
+                                        happens to be an astronaut? Yeah,
                                         neither are we. That&apos;s why
                                         you&apos;ll never see a full-sized
                                         luxury SUV for $79/mo on our site.
                                         Advertised prices are too good to be
-                                        true &dash; because they often are.
+                                        true - because they often are.
                                         Don&apos;t be fooled, at Deliver My Ride
                                         the price you see is the price
                                         you&apos;ll pay.
@@ -99,7 +97,7 @@ export default class extends React.PureComponent {
                         <SmallAndDown>
                             <Row className="no-gutters">
                                 <img
-                                    src="../../static/images/icons/promise-mag.png"
+                                    src="/static/images/icons/promise-mag.png"
                                     alt="Our promise mag"
                                 />
                                 <h5 className="text-uppercase">
@@ -108,18 +106,17 @@ export default class extends React.PureComponent {
                             </Row>
                             <Row>
                                 <Col>
-                                    <p>
+                                    <p className="text-sm">
                                         Are you a helicopter pilot, ski-patrol
                                         surgeon and recent college graduate that
-                                        happens to be an astronaut&quest; Yeah,
-                                        neither are we. That&apos;s why
-                                        you&apos;ll never see a full-sized
-                                        luxury SUV for $79/mo on our site.
-                                        Advertised prices are too good to be
-                                        true &dash; because they often are.
-                                        Don&apos;t be fooled, at Deliver My Ride
-                                        the price you see is the price
-                                        you&apos;ll pay.
+                                        happens to be an astronaut Yeah, neither
+                                        are we. That&apos;s why you&apos;ll
+                                        never see a full-sized luxury SUV for
+                                        $79/mo on our site. Advertised prices
+                                        are too good to be true - because they
+                                        often are. Don&apos;t be fooled, at
+                                        Deliver My Ride the price you see is the
+                                        price you&apos;ll pay.
                                     </p>
                                 </Col>
                             </Row>
@@ -131,24 +128,24 @@ export default class extends React.PureComponent {
                             <Row className="no-gutters">
                                 <Col md="1" sm="1" xs="1">
                                     <img
-                                        src="../../static/images/icons/promise-comp.png"
+                                        src="/static/images/icons/promise-comp.png"
                                         alt="Our promise comp"
                                     />
                                 </Col>
                                 <Col md="10">
                                     <h5 className="text-uppercase">
-                                        100&percnt; dealer inventory{' '}
+                                        100% dealer inventory{' '}
                                     </h5>
-                                    <p>
+                                    <p className="text-sm">
                                         Deliver My Ride is the modern way to buy
                                         or lease a new vehicle. We only show you
                                         real cars that are in local dealer
-                                        inventory &dash; right now. Our pricing,
+                                        inventory - right now. Our pricing,
                                         rebates and financing options,
                                         aren&apos;t estimates they are the real
                                         price you will pay all without having to
                                         leave your home. We even deliver (for
-                                        free&excl;) to your home or office.
+                                        free) to your home or office.
                                     </p>
                                 </Col>
                             </Row>
@@ -156,36 +153,29 @@ export default class extends React.PureComponent {
                         <SmallAndDown>
                             <Row className="no-gutters">
                                 <img
-                                    src="../../static/images/icons/promise-comp.png"
+                                    src="/static/images/icons/promise-comp.png"
                                     alt="Our promise comp"
                                 />
                                 <h5 className="text-uppercase">
-                                    100&percnt; dealer inventory{' '}
+                                    100% dealer inventory{' '}
                                 </h5>
                             </Row>
                             <Row>
                                 <Col>
-                                    <p>
+                                    <p className="text-sm">
                                         Deliver My Ride is the modern way to buy
                                         or lease a new vehicle. We only show you
                                         real cars that are in local dealer
-                                        inventory &dash; right now. Our pricing,
+                                        inventory - right now. Our pricing,
                                         rebates and financing options,
                                         aren&apos;t estimates they are the real
                                         price you will pay all without having to
                                         leave your home. We even deliver (for
-                                        free&excl;) to your home or office.
+                                        free) to your home or office.
                                     </p>
                                 </Col>
                             </Row>
                         </SmallAndDown>
-                    </Col>
-
-                    <Col className="d-flex justify-content-center">
-                        <Button color="primary" size="lg">
-                            {' '}
-                            See what our customers are saying{' '}
-                        </Button>
                     </Col>
                 </Row>
             </div>
