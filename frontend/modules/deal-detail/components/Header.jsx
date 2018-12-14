4b52fb600e7cae961578f6f22365df2f65a7115f
Lenny <430designs@gmail.com>
2018-12-14 10:34:15 -0500

    Stationary header markup and style for desktop done

--- frontend/modules/deal-detail/components/Header.jsx
+++ frontend/modules/deal-detail/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { dealType } from '../../../core/types';
 
-import { Row, Col } from 'reactstrap';
-import { faCalculator } from '@fortawesome/pro-light-svg-icons';
+import { Row, Col, Button } from 'reactstrap';
+import { faCalculator, faInfoCircle } from '@fortawesome/pro-light-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import strings from '../../../util/strings';
@@ -18,42 +18,66 @@ export default class Header extends React.PureComponent {
         return (
             <div>
                 <Row className="deal-details__new-header stationary">
-                    <Col sm="6">
-                        <div className="deal-details__title-year-make">
-                            {strings.dealYearMake(this.props.deal)}
+                    <Col sm="8">
+                        <div className="deal-details__new-header title-year-make">
+                            {this.props.deal.year} {this.props.deal.make}{' '}
+                            {this.props.deal.model}
                         </div>
-                        <div className="deal-details__title-model-trim">
-                            {strings.dealModelTrim(this.props.deal)}
+                        <div className="deal-details__new-header title-model-trim">
+                            {this.props.deal.series}
                         </div>
                     </Col>
-                    <Col sm="6" className="deal-details__payment">
-                        <p>
-                            <span className="monthly">$637/mo</span>
+                    <Col
+                        sm="4"
+                        className="deal-details__new-header payment d-flex"
+                    >
+                        <div className="d-inline-block monthly">
+                            <sup>$</sup>
+                            <b>637</b>
+                            <sub>/mo</sub>
+                        </div>
+                        <div className="configure d-inline-block">
                             <FontAwesomeIcon icon={faCalculator} />
                             Configure Payment
-                        </p>
+                        </div>
                     </Col>
                 </Row>
 
-                <Row className="deal-details__new-header fixed">
+                <Row className="deal-details__new-header new-header fixed hidden">
                     <Col sm="6">
-                        <div className="deal-details__title-year-make">
+                        <div className="deal-details__new-header title-year-make">
                             {strings.dealYearMake(this.props.deal)}
                         </div>
-                        <div className="deal-details__title-model-trim">
+                        <div className="deal-details__new-header title-model-trim">
                             {strings.dealModelTrim(this.props.deal)}
                         </div>
                     </Col>
                     <Row>
-                        <Col lg="4" className="border-right">
+                        <Col lg="auto" className="border-right">
                             <span className="dollar-sign">$</span>
                             <span className="payment">45,375</span>
+                            <br />
+                            <span className="tooltip">
+                                <FontAwesomeIcon icon={faInfoCircle} />
+                                DMR Price
+                            </span>
                         </Col>
-                        <Col lg="auto" sm="6" className="deal-details__payment">
+                        <Col
+                            lg="auto"
+                            sm="6"
+                            className="deal-details__new-header payment"
+                        >
                             <p>
-                                <span className="monthly">$637/mo</span>
+                                <span className="monthly">
+                                    $637
+                                    <sup>/mo</sup>
+                                </span>
+                                <br />
                                 <FontAwesomeIcon icon={faCalculator} />
-                                Configure Payment
+                                Configure
+                                <Button className="" color="primary">
+                                    <b>Next: </b> Begin Purchase
+                                </Button>
                             </p>
                         </Col>
                     </Row>
