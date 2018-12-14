c4ba19b44734b8cca1457e638bed9271a602513c
Leonard <430designs@gmail.com>
2018-12-14 00:11:43 -0500

    Placeholder

--- frontend/modules/deal-detail/components/Header.jsx
+++ frontend/modules/deal-detail/components/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { dealType } from '../../../core/types';
 
 import { Row, Col } from 'reactstrap';
+import { faCalculator } from '@fortawesome/pro-light-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import strings from '../../../util/strings';
 import DealStockNumber from '../../../components/Deals/DealStockNumber';
@@ -12,20 +14,51 @@ export default class Header extends React.PureComponent {
     };
 
     render() {
+        console.log(this.props.deal);
         return (
-            <Row className="deal-details__header">
-                <Col sm="6">
-                    <div className="deal-details__title-year-make">
-                        {strings.dealYearMake(this.props.deal)}
-                    </div>
-                    <div className="deal-details__title-model-trim">
-                        {strings.dealModelTrim(this.props.deal)}
-                    </div>
-                </Col>
-                <Col sm="6" className="deal-details__stock-number">
-                    <DealStockNumber deal={this.props.deal} />
-                </Col>
-            </Row>
+            <div>
+                <Row className="deal-details__new-header stationary">
+                    <Col sm="6">
+                        <div className="deal-details__title-year-make">
+                            {strings.dealYearMake(this.props.deal)}
+                        </div>
+                        <div className="deal-details__title-model-trim">
+                            {strings.dealModelTrim(this.props.deal)}
+                        </div>
+                    </Col>
+                    <Col sm="6" className="deal-details__payment">
+                        <p>
+                            <span className="monthly">$637/mo</span>
+                            <FontAwesomeIcon icon={faCalculator} />
+                            Configure Payment
+                        </p>
+                    </Col>
+                </Row>
+
+                <Row className="deal-details__new-header fixed">
+                    <Col sm="6">
+                        <div className="deal-details__title-year-make">
+                            {strings.dealYearMake(this.props.deal)}
+                        </div>
+                        <div className="deal-details__title-model-trim">
+                            {strings.dealModelTrim(this.props.deal)}
+                        </div>
+                    </Col>
+                    <Row>
+                        <Col lg="4" className="border-right">
+                            <span className="dollar-sign">$</span>
+                            <span className="payment">45,375</span>
+                        </Col>
+                        <Col lg="auto" sm="6" className="deal-details__payment">
+                            <p>
+                                <span className="monthly">$637/mo</span>
+                                <FontAwesomeIcon icon={faCalculator} />
+                                Configure Payment
+                            </p>
+                        </Col>
+                    </Row>
+                </Row>
+            </div>
         );
     }
 }
