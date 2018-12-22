1b20be6da73d6a600234aadb11dc666a8d04790a
Matt Wisner <matt@detroitweb.company>
2018-12-22 09:35:11 -0600

    test get user

---
+++ backend/tests/Feature/Api/AuthGetUserTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace Tests\Feature\Api;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Tests\TestCaseWithAuth;
+use Laravel\Passport\Passport;
+
+class AuthGetUserTest extends TestCaseWithAuth
+{
+
+    /** @test */
+    public function it_works()
+    {
+        $user = factory(User::class)->make();
+        $user->save();
+        Passport::actingAs($user);
+
+        $payload = [
+            'email' => $user->email,
+            'password' => 'myfakepassword'
+        ];
+
+        $response = $this->json('GET', 'api/auth/user', $payload);
+        $response
+            ->assertJsonStructure(
+                [
+                    'first_name',
+                    'last_name',
+                    'email',
+                ]
+            )
+            ->assertStatus(200);
+    }
+
+    /** @test */
+    public function it_does_not_allow_anon()
+    {
+        $user = factory(User::class)->make();
+        $user->save();
+
+        $payload = [
+            'email' => $user->email,
+            'password' => 'myfakepassword'
+        ];
+
+        $response = $this->json('GET', 'api/auth/user', $payload);
+        $response
+            ->assertStatus(401);
+    }
+}
