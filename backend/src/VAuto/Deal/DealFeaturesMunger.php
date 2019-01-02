051a0402597f0e152d29e406257c90a75601b65e
Chris Povinelli <cpovinelli85@gmail.com>
2019-01-02 11:53:40 -0500

    Removed jatoFeatures and dealJatoFeatures from site and models

--- backend/src/VAuto/Deal/DealFeaturesMunger.php
+++ backend/src/VAuto/Deal/DealFeaturesMunger.php
@@ -3,7 +3,6 @@
 namespace DeliverMyRide\VAuto\Deal;
 
 use App\Models\Deal;
-use App\Models\JatoFeature;
 use DeliverMyRide\JATO\JatoClient;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
@@ -46,24 +45,11 @@ class DealFeaturesMunger
             'feature_skipped' => 'Yes',
         ];
 
-        if ($force) {
-            $this->deal->jatoFeatures()->sync([]);
-        }
-
-        if ($this->deal->jatoFeatures()->count()) {
-            return $this->debug;
-        }
-
         $this->debug['feature_skipped'] = 'No';
 
         //
         // Get get data sources
         $this->features = $this->fetchVersionFeatures();
-
-        //
-        // Do feature things
-        $this->saveDealJatoFeatures();
-        $this->saveCustomHackyJatoFeatures();
 
         return $this->debug;
     }
@@ -78,104 +64,6 @@ class DealFeaturesMunger
             return collect($this->client->feature->get($this->version->jato_vehicle_id, '', 1, 400)->results);
         } catch (ServerException | ClientException $e) {
             return collect([]);
-        }
-    }
-
-    private function getCategorizedFeaturesByVehicleId()
-    {
-        $data = [];
-
-        foreach ($this->features->all() as $feature) {
-            if (! isset($data[$feature->categoryId])) {
-                $data[$feature->categoryId] = [];
-            }
-            $data[$feature->categoryId][] = $feature;
-        }
-
-        return $data;
-    }
-
-    private function saveDealJatoFeatures()
-    {
-        $features = $this->getCategorizedFeaturesByVehicleId();
-
-        foreach (JatoFeature::SYNC_GROUPS as $group) {
-            if (isset($features[$group['id']])) {
-                $this->saveDealJatoFeaturesByGroup($features[$group['id']], $group['title']);
-            }
-        }
-    }
-
-    private function saveDealJatoFeaturesByGroup(array $features, string $group)
-    {
-        collect($features)
-            ->reduce(function (Collection $carry, $jatoFeature) {
-                return $carry->merge(self::splitJATOFeaturesAndContent($jatoFeature->feature, $jatoFeature->content));
-            }, collect())
-            ->each(function ($featureAndContent) use ($group) {
-
-                /*
-                 * Only interior features that contain "seat" should be added to seating
-                 */
-                if ($group === JatoFeature::GROUP_SEATING_KEY && ! str_contains($featureAndContent['feature'], 'seat')) {
-                    return;
-                }
-
-                /*
-                 * Only add features that have _content_ that starts with "Standard", "Yes".
-                 */
-                if (starts_with($featureAndContent['content'], ['Standard', 'Yes'])) {
-                    try {
-                        $feature = JatoFeature::updateOrCreate([
-                            'feature' => $featureAndContent['feature'],
-                            'content' => $featureAndContent['content'],
-                        ], [
-                            'feature' => $featureAndContent['feature'],
-                            'content' => $featureAndContent['content'],
-                            'group' => $this->getGroupWithOverrides($featureAndContent['feature'], $group),
-                        ]);
-
-                        $feature->deals()->save($this->deal);
-                        $this->debug['feature_count']++;
-                    } catch (QueryException $e) {
-                        // Already saved.
-                    }
-                }
-            });
-    }
-
-    private function getGroupWithOverrides(string $feature, string $group)
-    {
-        return str_contains($feature, 'seat') ? JatoFeature::GROUP_SEATING_KEY : $group;
-    }
-
-    private function saveCustomHackyJatoFeatures()
-    {
-        if ($this->version->body_style === 'Pickup') {
-            try {
-                $doorCount = JatoFeature::updateOrCreate([
-                    'feature' => "$this->deal->door_count Door",
-                    'content' => $this->deal->door_count,
-                ], [
-                    'feature' => "$this->deal->door_count Door",
-                    'content' => $this->deal->door_count,
-                    'group' => JatoFeature::GROUP_TRUCK_KEY,
-                ]);
-
-                $cabType = JatoFeature::updateOrCreate([
-                    'feature' => "{$this->version->cab} Cab",
-                    'content' => $this->version->cab,
-                ], [
-                    'feature' => "{$this->version->cab} Cab",
-                    'content' => $$this->version->cab,
-                    'group' => JatoFeature::GROUP_TRUCK_KEY,
-                ]);
-
-                $doorCount->deals()->save($this->deal);
-                $cabType->deals()->save($this->deal);
-            } catch (QueryException $e) {
-                // Already Saved.
-            }
         }
     }
 
