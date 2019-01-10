a1a44e36746ed011844c6c2569a08a7c0f15acf1
Matt Wisner <matt@detroitweb.company>
2019-01-10 16:24:00 -0500

    oops

--- frontend/pages/deal-detail.js
+++ frontend/pages/deal-detail.js
@@ -26,7 +26,7 @@ export default class Page extends React.Component {
                         </React.Fragment>
                     )}
                 </Head>
-                <DealDetail initialQuoteParams={this.props.query} />
+                <DealDetail />
             </React.Fragment>
         );
     }
