8091dbb27a868966142ba8600f2e4e954cabbce9
Matt Wisner <matt@detroitweb.company>
2019-01-10 10:19:40 -0500

    Fixing circle

--- backend/tests/Feature/Api/Order/CheckoutStartTest.php
+++ backend/tests/Feature/Api/Order/CheckoutStartTest.php
@@ -9,15 +9,13 @@ use App\Models\User;
 
 class CheckoutStartTest extends TestCaseWithAuth
 {
-    private $payload = [];
-
-    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    /** @test */
+    public function it_works_as_anon()
     {
-        parent::__construct($name, $data, $dataName);
 
         $deal = factory(Deal::class)->create();
 
-        $this->payload = [
+        $payload = [
             'deal_id' => $deal->id,
             'strategy' => 'lease',
             'quote' => (new FakeQuote())->get(),
@@ -35,13 +33,9 @@ class CheckoutStartTest extends TestCaseWithAuth
                 'estimate' => null,
             ],
         ];
-    }
 
-    /** @test */
-    public function it_works_as_anon()
-    {
         $response = $this
-            ->json('POST', 'api/checkout/start', $this->payload);
+            ->json('POST', 'api/checkout/start', $payload);
 
         $response
             ->assertStatus(200)
@@ -58,10 +52,29 @@ class CheckoutStartTest extends TestCaseWithAuth
     public function it_works_as_logged_in_user()
     {
         $user = factory(User::class)->create();
+        $deal = factory(Deal::class)->create();
 
+        $payload = [
+            'deal_id' => $deal->id,
+            'strategy' => 'lease',
+            'quote' => (new FakeQuote())->get(),
+            'amounts' => (object) [
+                'role' => 'dmr',
+                'price' => '21926.00',
+                'leased_annual_mileage' => 10000,
+                'term' => 36,
+                'monthly_payment' => '121.62',
+                'down_payment' => '2283.66',
+            ],
+            'trade' => (object) [
+                'value' => 0,
+                'owed' => 0,
+                'estimate' => null,
+            ],
+        ];
         $response = $this
             ->actingAs($user)
-            ->json('POST', 'api/checkout/start', $this->payload);
+            ->json('POST', 'api/checkout/start', $payload);
 
         $response
             ->assertStatus(200)
