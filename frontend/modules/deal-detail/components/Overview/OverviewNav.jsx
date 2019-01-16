b7d72ef6eff098db20e31d197d08c6e8b0ee9d85
Leonard <430designs@gmail.com>
2019-01-16 08:55:19 -0500

    Placeholder

--- frontend/modules/deal-detail/components/Overview/OverviewNav.jsx
+++ frontend/modules/deal-detail/components/Overview/OverviewNav.jsx
@@ -17,7 +17,61 @@ export default class extends React.PureComponent {
         window.removeEventListener('scroll', this.handleScroll);
     }
 
-    handleScroll() {}
+    handleScroll() {
+        let mainNavLinks = document.querySelectorAll(
+            '.section-header-overview'
+        );
+        let mainSections = document.querySelectorAll('.deal__section-heading');
+
+        // let  div = document.getElementById("overview");
+        // let  rect = div.getBoundingClientRect();
+        // let y = rect.top;
+
+        // console.log(y);
+
+        window.addEventListener('scroll', event => {
+            let fromTop = window.scrollY;
+
+            mainNavLinks.forEach(link => {
+                let section = document.querySelector(link.id);
+                let sectionFromTop = section.getBoundingClientRect();
+                let y = rect.top;
+
+                console.log(y);
+
+                if (
+                    section.offsetTop <= fromTop &&
+                    section.offsetTop + section.offsetHeight > fromTop
+                ) {
+                    link.classList.add('current');
+                    // console.log("Did we do it");
+                } else {
+                    link.classList.remove('current');
+                    // console.log("Or didn't we");
+                }
+            });
+        });
+
+        /*window.addEventListener("scroll", event => {
+            let overviewNav = document.getElementsByClassName("section-header-overview");
+            let fromTop = window.scrollY;
+
+            overviewNav.forEach(link => {
+                let section = document.querySelector(this.link.id);
+                if(section){
+                    console.log(section);
+                }else{
+                    console.log("No section!");
+                }
+                // if ( section.scrollTop <= fromTop && section.scrollTop + section.offsetHeight > fromTop) {
+                //     link.classList.add("current");
+                // } else {
+                //     link.classList.remove("current");
+                // }
+            });
+        });
+*/
+    }
 
     render() {
         return (
@@ -36,32 +90,43 @@ export default class extends React.PureComponent {
                             <Container>
                                 <Row className="deal__section-heading">
                                     <Col>
-                                        <h6 className="section-header text-center mb-0">
-                                            {' '}
-                                            Overview{' '}
+                                        <h6
+                                            id="overview"
+                                            className="section-header-overview text-center mb-0"
+                                        >
+                                            Overview
                                         </h6>
                                     </Col>
                                     <Col>
-                                        <h6 className="section-header text-center mb-0">
+                                        <h6
+                                            id="specs"
+                                            className="section-header-overview text-center mb-0"
+                                        >
                                             {' '}
                                             Specs{' '}
                                         </h6>
                                     </Col>
                                     <Col>
-                                        <h6 className="section-header text-center mb-0">
-                                            {' '}
-                                            Additional Information{' '}
+                                        <h6
+                                            id="additional-information"
+                                            className="section-header-overview text-center mb-0"
+                                        >
+                                            Additional Information
                                         </h6>
                                     </Col>
                                     <Col>
-                                        <h6 className="section-header text-center mb-0">
-                                            {' '}
+                                        <h6
+                                            id="our-promise"
+                                            className="section-header-overview text-center mb-0"
+                                        >
                                             Our Promise
                                         </h6>
                                     </Col>
                                     <Col>
-                                        <h6 className="section-header text-center mb-0 border-0">
-                                            {' '}
+                                        <h6
+                                            id="faqs"
+                                            className="section-header-overview text-center mb-0 border-0"
+                                        >
                                             FAQs
                                         </h6>
                                     </Col>
